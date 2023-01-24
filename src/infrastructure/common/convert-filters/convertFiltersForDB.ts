6e41ff441d9171e54b289da566a95578afd1133d
sergehall <sergioartg@gmail.com>
2023-01-24 04:21:30 -0800

    refactoring ConvertFiltersForDB

--- src/infrastructure/common/convert-filters/convertFiltersForDB.ts
+++ src/infrastructure/common/convert-filters/convertFiltersForDB.ts
@@ -13,43 +13,48 @@ export class ConvertFiltersForDB {
 
   async _forMongo(queryFilters: any, pathFilterEnum: PatternConvertFilterType) {
     const convertedFilters = [];
-    for (let i = 0, l = Object.keys(queryFilters).length; i < l; i++) {
-      for (const key in queryFilters[i]) {
-        if (pathFilterEnum.hasOwnProperty(key) && queryFilters[i][key]) {
-          const convertedFilter = {};
-          if (key === 'banStatus' && queryFilters[i][key] === 'true') {
-            queryFilters[i][key] = true;
+    try {
+      for (let i = 0, l = Object.keys(queryFilters).length; i < l; i++) {
+        for (const key in queryFilters[i]) {
+          if (pathFilterEnum.hasOwnProperty(key) && queryFilters[i][key]) {
+            const convertedFilter = {};
+            if (key === 'banStatus' && queryFilters[i][key] === 'true') {
+              queryFilters[i][key] = true;
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              convertedFilter[pathFilterEnum[key]] = {
+                $eq: queryFilters[i][key],
+              };
+              convertedFilters.push(convertedFilter);
+              continue;
+            }
+            if (key === 'banStatus' && queryFilters[i][key] === 'false') {
+              queryFilters[i][key] = false;
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              convertedFilter[pathFilterEnum[key]] = {
+                $eq: queryFilters[i][key],
+              };
+              convertedFilters.push(convertedFilter);
+              continue;
+            }
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             convertedFilter[pathFilterEnum[key]] = {
-              $eq: queryFilters[i][key],
+              $regex: queryFilters[i][key].toLowerCase(),
+              $options: 'i',
             };
             convertedFilters.push(convertedFilter);
-            continue;
           }
-          if (key === 'banStatus' && queryFilters[i][key] === 'false') {
-            queryFilters[i][key] = false;
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            convertedFilter[pathFilterEnum[key]] = {
-              $eq: queryFilters[i][key],
-            };
-            convertedFilters.push(convertedFilter);
-            continue;
-          }
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          convertedFilter[pathFilterEnum[key]] = {
-            $regex: queryFilters[i][key].toLowerCase(),
-            $options: 'i',
-          };
-          convertedFilters.push(convertedFilter);
         }
       }
+      if (convertedFilters.length === 0) {
+        convertedFilters.push({});
+      }
+      return convertedFilters;
+    } catch (err) {
+      console.log(err);
+      return [{}];
     }
-    if (convertedFilters.length === 0) {
-      convertedFilters.push({});
-    }
-    return convertedFilters;
   }
 }
