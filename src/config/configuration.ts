bc7852c420382c7442227f9451a3f68e1dbfa7db
sergehall <sergioartg@gmail.com>
2023-01-30 20:26:59 -0800

    refactoring databaseProviders

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,10 +2,14 @@ export const getConfiguration = () => {
   return {
     ENV: process.env.NODE_ENV || 'development',
     mongoose: {
-      URI_DATABASE:
-        process.env.ATLAS_URI ||
-        process.env.MONGO_URI + '/' + process.env.NEST_DATABASE ||
-        process.env.TEST_DATABASE,
+      uri: {
+        ATLAS_URI: process.env.ATLAS_URI,
+        MONGO_URI: process.env.MONGO_URI,
+        TEST_DATABASE: process.env.TEST_DATABASE,
+      },
+    },
+    database: {
+      NEST_DATABASE: process.env.NEST_DATABASE,
     },
   };
 };
--- src/infrastructure/database/database.providers.ts
+++ src/infrastructure/database/database.providers.ts
@@ -6,21 +6,14 @@ export const databaseProviders = [
   {
     provide: ConnectionEnums.ASYNC_CONNECTION,
     useFactory: async () => {
-      const uri_database = getConfiguration().mongoose.URI_DATABASE;
-      if (uri_database) {
-        const connection = await createConnection(uri_database);
+      const uri = getConfiguration().mongoose.uri.ATLAS_URI;
+      const database = getConfiguration().database.NEST_DATABASE;
+      if (uri && database) {
+        const connection = await createConnection(uri + '/' + database);
         console.log('Mongoose connected.');
         return connection;
       }
       throw new BadGatewayException();
     },
   },
-
-  // {
-  //   provide: ConnectionEnums.DATABASE_CONNECTION,
-  //   useFactory: async (): Promise<typeof mongoose> =>
-  //     await mongoose.connect(
-  //       process.env.ATLAS_URI + '/' + process.env.NEST_DATABASE,
-  //     ),
-  // },
 ];
