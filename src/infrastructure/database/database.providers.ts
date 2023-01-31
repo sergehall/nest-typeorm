3150daad5de59185cf266e768e0675fe7aee4364
sergehall <sergioartg@gmail.com>
2023-01-30 23:47:49 -0800

    add env-names.enums

--- src/infrastructure/database/database.providers.ts
+++ src/infrastructure/database/database.providers.ts
@@ -11,8 +11,8 @@ export const databaseProviders = [
       const uri = getConfiguration().mongoose.uri.ATLAS_URI;
       const ENV = getConfiguration().ENV;
       let database = getConfiguration().database.DEV_DATABASE;
-      if (ENV === EnvNamesEnums.PRODUCTION) {
-        database = getConfiguration().database.PROD_NEST_DATABASE;
+      if (ENV === EnvNamesEnums.DEVELOPMENT) {
+        database = getConfiguration().database.DEV_DATABASE;
       }
       if (uri && database) {
         const connection = await createConnection(uri + '/' + database);
