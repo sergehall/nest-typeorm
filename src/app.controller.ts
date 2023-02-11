868b5306a440ab6a4d7331982075e05a6b5fc946
sergehall <sergioartg@gmail.com>
2023-02-11 05:40:43 -0800

    refactoring to features

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Get()
   async getHello(): Promise<string> {
     return this.appService.getHello();
