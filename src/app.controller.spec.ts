1b2dd0c52552c6e33307fe3b200fd557385f39cd
sergehall <sergioartg@gmail.com>
2023-03-03 23:58:59 -0800

    test   describe('root'....

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      expect.assertions(1);
+      // Act
+      const getHello = await appController.getHello();
+      // Assert
+      expect(getHello).toEqual('Hello World!');
     });
   });
 });
