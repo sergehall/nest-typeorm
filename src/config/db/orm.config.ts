7dfbabd70ac50bf89c8177517e9af2c195ebe679
sergehall <sergioartg@gmail.com>
2023-06-22 02:15:53 -0700

    Connection to posgresql heroku done

--- src/config/db/orm.config.ts
+++ src/config/db/orm.config.ts
@@ -1,34 +1,33 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { getConfiguration } from '../configuration';
-import { EnvNamesEnums } from '../throttle/enums/env-names.enums';
+import { AuthPgEntity } from '../../auth-pg/auth-pg.entity';
 
-// export const ormConfig: TypeOrmModuleOptions = {
-//   type: 'postgres' as const,
-//   host: getConfiguration().db.pg.host.heroku.PG_HOST_HEROKU,
-//   port: getConfiguration().db.pg.port.PG_PORT,
-//   username: getConfiguration().auth.PG_HEROKU_USER_NAME,
-//   password: getConfiguration().auth.PG_HEROKU_USER_PASSWORD,
-//   database: getConfiguration().db.nameDatabase.PG_NEST_HEROKU_DATABASE,
-//   ssl:
-//     getConfiguration().ENV === EnvNamesEnums.DEVELOPMENT
-//       ? false
-//       : { rejectUnauthorized: false },
-//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//   synchronize: false,
-//   logging: false,
-// };
-export const ormConfig: TypeOrmModuleOptions = {
-  type: 'postgres' as const,
-  host: getConfiguration().db.pg.host.local.PG_URI_LOCAL,
-  port: getConfiguration().db.pg.port.PG_PORT,
-  username: 'serge-nodejs',
-  password: getConfiguration().auth.PG_LOCAL_USER_PASSWORD,
-  database: 'Local-Nest-pg-DB',
-  ssl:
-    getConfiguration().ENV === EnvNamesEnums.DEVELOPMENT
-      ? false
-      : { rejectUnauthorized: false },
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: false,
-  logging: false,
+export const ormConfig: {
+  localHost: TypeOrmModuleOptions;
+  herokuPosgreSql: TypeOrmModuleOptions;
+} = {
+  localHost: {
+    type: 'postgres' as const,
+    host: getConfiguration().db.pg.host.local.PG_URI_LOCAL,
+    port: getConfiguration().db.pg.port.PG_PORT,
+    username: 'serge-nodejs',
+    password: getConfiguration().auth.PG_LOCAL_USER_PASSWORD,
+    database: 'Local-Nest-pg-DB',
+    ssl: { rejectUnauthorized: false },
+    entities: [AuthPgEntity],
+    synchronize: true,
+    logging: false,
+  },
+  herokuPosgreSql: {
+    type: 'postgres' as const,
+    host: getConfiguration().db.pg.host.heroku.PG_HOST_HEROKU,
+    port: getConfiguration().db.pg.port.PG_PORT,
+    username: getConfiguration().auth.PG_HEROKU_USER_NAME,
+    password: getConfiguration().auth.PG_HEROKU_USER_PASSWORD,
+    database: getConfiguration().db.nameDatabase.PG_NEST_HEROKU_DATABASE,
+    ssl: { rejectUnauthorized: false },
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    logging: false,
+  },
 };
