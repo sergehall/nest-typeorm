f6d48ada4a8587f7575f382655d9959efc4ca07b
sergehall <sergioartg@gmail.com>
2023-07-09 01:55:57 -0700

    add create comments by raw sql

--- src/features/comments/application/use-cases/create-comment.use-case.ts
+++ src/features/comments/application/use-cases/create-comment.use-case.ts
@@ -1,5 +1,4 @@
 import { CreateCommentDto } from '../../dto/create-comment.dto';
-import { CommentsEntity } from '../../entities/comments.entity';
 import {
   ForbiddenException,
   InternalServerErrorException,
@@ -7,12 +6,13 @@ import {
 } from '@nestjs/common';
 import * as uuid4 from 'uuid4';
 import { StatusLike } from '../../../../infrastructure/database/enums/like-status.enums';
-import { PostsService } from '../../../posts/application/posts.service';
-import { CommentsRepository } from '../../infrastructure/comments.repository';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CommentsReturnEntity } from '../../entities/comments-return.entity';
 import { CurrentUserDto } from '../../../users/dto/currentUser.dto';
-import { BloggerBlogsRepository } from '../../../blogger-blogs/infrastructure/blogger-blogs.repository';
+import { PostsRawSqlRepository } from '../../../posts/infrastructure/posts-raw-sql.repository';
+import { BloggerBlogsRawSqlRepository } from '../../../blogger-blogs/infrastructure/blogger-blogs-raw-sql.repository';
+import { TablesCommentsRawSqlEntity } from '../../entities/tables-comments-raw-sql.entity';
+import { CommentsRawSqlRepository } from '../../infrastructure/comments-raw-sql.repository';
 
 export class CreateCommentCommand {
   constructor(
@@ -26,59 +26,48 @@ export class CreateCommentUseCase
   implements ICommandHandler<CreateCommentCommand>
 {
   constructor(
-    protected postsService: PostsService,
-    protected commentsRepository: CommentsRepository,
-    protected bloggerBlogsRepository: BloggerBlogsRepository,
+    protected postsRawSqlRepository: PostsRawSqlRepository,
+    protected bloggerBlogsRawSqlRepository: BloggerBlogsRawSqlRepository,
+    protected commentsRawSqlRepository: CommentsRawSqlRepository,
   ) {}
   async execute(command: CreateCommentCommand): Promise<CommentsReturnEntity> {
-    const post = await this.postsService.checkPostInDB(command.postId);
+    const post = await this.postsRawSqlRepository.findPostByPostId(
+      command.postId,
+    );
     if (!post) throw new NotFoundException();
     const isBannedUserForBlog =
-      await this.bloggerBlogsRepository.isBannedUserForBlog(
+      await this.bloggerBlogsRawSqlRepository.isBannedUserForBlog(
         command.currentUser.id,
         post.blogId,
       );
     if (isBannedUserForBlog) throw new ForbiddenException();
-    const newComment: CommentsEntity = {
+    const newComment: TablesCommentsRawSqlEntity = {
       id: uuid4().toString(),
       content: command.createCommentDto.content,
       createdAt: new Date().toISOString(),
-      postInfo: {
-        id: post.id,
-        title: post.title,
-        blogId: post.blogId,
-        blogName: post.blogName,
-        blogOwnerId: post.postOwnerInfo.userId,
-      },
-      commentatorInfo: {
-        userId: command.currentUser.id,
-        userLogin: command.currentUser.login,
-        isBanned: false,
-      },
-      likesInfo: {
-        likesCount: 0,
-        dislikesCount: 0,
-        myStatus: StatusLike.NONE,
-      },
-      banInfo: {
-        isBanned: false,
-        banDate: null,
-        banReason: null,
-      },
+      postInfoId: post.id,
+      postInfoTitle: post.title,
+      postInfoBlogId: post.blogId,
+      postInfoBlogName: post.blogName,
+      postInfoBlogOwnerId: post.postOwnerId,
+      commentatorInfoUserId: command.currentUser.id,
+      commentatorInfoUserLogin: command.currentUser.login,
+      commentatorInfoIsBanned: false,
+      banInfoIsBanned: false,
+      banInfoBanDate: null,
+      banInfoBanReason: null,
     };
-    const createComment = await this.commentsRepository.createComment(
-      post.blogId,
-      command.postId,
+    const createComment = await this.commentsRawSqlRepository.createComment(
       newComment,
     );
     if (!createComment) throw new InternalServerErrorException();
     return {
-      id: newComment.id,
-      content: newComment.content,
-      createdAt: newComment.createdAt,
+      id: createComment.id,
+      content: createComment.content,
+      createdAt: createComment.createdAt,
       commentatorInfo: {
-        userId: newComment.commentatorInfo.userId,
-        userLogin: newComment.commentatorInfo.userLogin,
+        userId: createComment.commentatorInfoUserId,
+        userLogin: createComment.commentatorInfoUserLogin,
       },
       likesInfo: {
         likesCount: 0,
