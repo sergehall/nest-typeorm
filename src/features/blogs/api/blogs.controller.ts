627f7ed44bdd2dea050110af2190da44284a6ce9
sergehall <sergioartg@gmail.com>
2023-07-12 20:05:09 -0700

    move openFindBlogById with raw sql flow

--- src/features/blogs/api/blogs.controller.ts
+++ src/features/blogs/api/blogs.controller.ts
@@ -2,16 +2,15 @@ import {
   Controller,
   Get,
   Param,
-  NotFoundException,
   Query,
   UseGuards,
   Request,
 } from '@nestjs/common';
 import { BlogsService } from '../application/blogs.service';
 import { SkipThrottle } from '@nestjs/throttler';
-import { BloggerBlogsEntity } from '../../blogger-blogs/entities/blogger-blogs.entity';
 import { IdParams } from '../../common/params/id.params';
 import { ParseQuery } from '../../common/parse-query/parse-query';
+
 import { PaginationTypes } from '../../common/pagination/types/pagination.types';
 import { NoneStatusGuard } from '../../auth/guards/none-status.guard';
 import { CheckAbilities } from '../../../ability/abilities.decorator';
@@ -20,6 +19,7 @@ import { User } from '../../users/infrastructure/schemas/user.schema';
 import { CurrentUserDto } from '../../users/dto/currentUser.dto';
 import { PostsService } from '../../posts/application/posts.service';
 import { BlogIdParams } from '../../common/params/blogId.params';
+import { ReturnBloggerBlogsEntity } from '../../blogger-blogs/entities/return-blogger-blogs.entity';
 
 @SkipThrottle()
 @Controller('blogs')
@@ -38,12 +38,8 @@ export class BlogsController {
   @Get(':id')
   async openFindBlogById(
     @Param() params: IdParams,
-  ): Promise<BloggerBlogsEntity | null> {
-    const blog = await this.blogsService.openFindBlogById(params.id);
-    if (!blog) {
-      throw new NotFoundException();
-    }
-    return blog;
+  ): Promise<ReturnBloggerBlogsEntity> {
+    return await this.blogsService.openFindBlogById(params.id);
   }
   @Get(':blogId/posts')
   @UseGuards(NoneStatusGuard)
