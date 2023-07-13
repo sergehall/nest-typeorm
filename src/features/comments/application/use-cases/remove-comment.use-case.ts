568a141152db4945fc7c502270d6721e161e128a
sergehall <sergioartg@gmail.com>
2023-07-12 21:20:25 -0700

    move removeComment to raw-sql flow

--- src/features/comments/application/use-cases/remove-comment.use-case.ts
+++ src/features/comments/application/use-cases/remove-comment.use-case.ts
@@ -1,10 +1,14 @@
-import { ForbiddenException, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ForbiddenError } from '@casl/ability';
 import { Action } from '../../../../ability/roles/action.enum';
-import { CommentsRepository } from '../../infrastructure/comments.repository';
 import { CaslAbilityFactory } from '../../../../ability/casl-ability.factory';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CurrentUserDto } from '../../../users/dto/currentUser.dto';
+import { CommentsRawSqlRepository } from '../../infrastructure/comments-raw-sql.repository';
 
 export class RemoveCommentCommand {
   constructor(
@@ -18,11 +22,11 @@ export class RemoveCommentUseCase
   implements ICommandHandler<RemoveCommentCommand>
 {
   constructor(
-    protected commentsRepository: CommentsRepository,
+    protected commentsRawSqlRepository: CommentsRawSqlRepository,
     protected caslAbilityFactory: CaslAbilityFactory,
   ) {}
   async execute(command: RemoveCommentCommand) {
-    const findComment = await this.commentsRepository.findCommentById(
+    const findComment = await this.commentsRawSqlRepository.findCommentById(
       command.commentId,
     );
     if (!findComment) throw new NotFoundException();
@@ -31,13 +35,17 @@ export class RemoveCommentUseCase
         id: command.currentUserDto.id,
       });
       ForbiddenError.from(ability).throwUnlessCan(Action.DELETE, {
-        id: findComment.commentatorInfo.userId,
+        id: findComment.commentatorInfoUserId,
       });
-      return this.commentsRepository.removeComment(command.commentId);
+      return this.commentsRawSqlRepository.removeComment(command.commentId);
     } catch (error) {
       if (error instanceof ForbiddenError) {
         throw new ForbiddenException(error.message);
       }
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+      throw new InternalServerErrorException(error.message);
     }
   }
 }
