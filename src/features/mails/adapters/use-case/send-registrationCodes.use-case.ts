5f2013c09eeb76955e280b8f8b9fa9fa90c9898f
sergehall <sergioartg@gmail.com>
2023-07-30 04:00:38 -0700

    refactoring send-registrationCodes.use-case.ts

--- src/features/mails/adapters/use-case/send-registrationCodes.use-case.ts
+++ src/features/mails/adapters/use-case/send-registrationCodes.use-case.ts
@@ -19,7 +19,7 @@ export class SendRegistrationCodesUseCase
   async execute(command: SendRegistrationCodesCommand): Promise<void> {
     const EMAIL_FROM = this.mailerConfig.getNodeMailerEmail('NODEMAILER_EMAIL');
     const { email, confirmationCode } = command.emailAndCode;
-    const domainName = DomainNamesEnums.NEST_RAQ_SQL_URL;
+    const domainName = DomainNamesEnums.HEROKU_POSTGRES;
     const path = '/auth/confirm-registration';
     const parameter = '?code=' + confirmationCode;
     const fullURL = domainName + path + parameter;
