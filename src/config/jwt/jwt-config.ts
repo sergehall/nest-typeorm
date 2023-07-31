342ce51ccfc094f558a6f5c94ad571a3cf47e13a
sergehall <sergioartg@gmail.com>
2023-07-31 05:09:05 -0700

    refactoring Jwt Strategy

--- src/config/jwt/jwt-config.ts
+++ src/config/jwt/jwt-config.ts
@@ -3,16 +3,16 @@ import { BaseConfig } from '../base/base-config';
 
 @Injectable()
 export class JwtConfig extends BaseConfig {
-  async getAccSecretKey(): Promise<string> {
-    return await this.getValueString('ACCESS_SECRET_KEY', 'SECRET_KEY');
+  getAccSecretKey(): string {
+    return this.getValueString('ACCESS_SECRET_KEY', 'SECRET_KEY');
   }
-  async getRefSecretKey(): Promise<string> {
-    return await this.getValueString('REFRESH_SECRET_KEY', 'SECRET_KEY');
+  getRefSecretKey(): string {
+    return this.getValueString('REFRESH_SECRET_KEY', 'SECRET_KEY');
   }
-  async getExpAccTime(): Promise<string> {
-    return await this.getValueString('EXP_ACC_TIME', '300s');
+  getExpAccTime(): string {
+    return this.getValueString('EXP_ACC_TIME', '300s');
   }
-  async getExpRefTime(): Promise<string> {
-    return await this.getValueString('EXP_REF_TIME', '600s');
+  getExpRefTime(): string {
+    return this.getValueString('EXP_REF_TIME', '600s');
   }
 }
