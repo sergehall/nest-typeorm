7253fc4f51b829e7df51411a0952cc7f41ab5dc7
sergehall <sergioartg@gmail.com>
2023-07-30 18:21:17 -0700

    add DecodeTokenService for all app

--- src/features/security-devices/api/security-devices.controller.ts
+++ src/features/security-devices/api/security-devices.controller.ts
@@ -15,34 +15,42 @@ import { SkipThrottle } from '@nestjs/throttler';
 import { RemoveDevicesExceptCurrentCommand } from '../application/use-cases/remove-devices-exceptCurrent.use-case';
 import { CommandBus } from '@nestjs/cqrs';
 import { RemoveDevicesByDeviceIdCommand } from '../application/use-cases/remove-devices-byDeviceId.use-case';
-import jwt_decode from 'jwt-decode';
 import { DeviceIdParams } from '../../common/params/deviceId.params';
 import { ReturnSecurityDeviceEntity } from '../entities/return-security-device.entity';
+import { DecodeTokenService } from '../../../config/jwt/decode.service/decode-token-service';
 
 @SkipThrottle()
 @Controller('security')
 export class SecurityDevicesController {
   constructor(
     private readonly securityDevicesService: SecurityDevicesService,
+    private readonly decodeTokenService: DecodeTokenService,
     private commandBus: CommandBus,
   ) {}
+
   @UseGuards(CookiesJwtVerificationGuard)
   @Get('devices')
   async findDevices(
     @Request() req: any,
   ): Promise<ReturnSecurityDeviceEntity[]> {
-    const currentPayload: PayloadDto = jwt_decode(req.cookies.refreshToken);
+    const currentPayload: PayloadDto =
+      await this.decodeTokenService.toExtractPayload(req.cookies.refreshToken);
+
     return this.securityDevicesService.findDevices(currentPayload);
   }
+
   @HttpCode(HttpStatus.NO_CONTENT)
   @UseGuards(CookiesJwtVerificationGuard)
   @Delete('devices')
   async removeDevicesExceptCurrent(@Request() req: any): Promise<boolean> {
-    const currentPayload: PayloadDto = jwt_decode(req.cookies.refreshToken);
+    const currentPayload: PayloadDto =
+      await this.decodeTokenService.toExtractPayload(req.cookies.refreshToken);
+
     return await this.commandBus.execute(
       new RemoveDevicesExceptCurrentCommand(currentPayload),
     );
   }
+
   @HttpCode(HttpStatus.NO_CONTENT)
   @UseGuards(CookiesJwtVerificationGuard)
   @Delete('/devices/:deviceId')
@@ -50,7 +58,9 @@ export class SecurityDevicesController {
     @Request() req: any,
     @Param() params: DeviceIdParams,
   ): Promise<boolean> {
-    const currentPayload: PayloadDto = jwt_decode(req.cookies.refreshToken);
+    const currentPayload: PayloadDto =
+      await this.decodeTokenService.toExtractPayload(req.cookies.refreshToken);
+
     return await this.commandBus.execute(
       new RemoveDevicesByDeviceIdCommand(params.deviceId, currentPayload),
     );
