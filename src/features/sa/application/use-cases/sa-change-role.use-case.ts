5550858ef02ed04604a76db5d991f47bfe1527dc
sergehall <sergioartg@gmail.com>
2023-08-02 22:42:29 -0700

    refactoring users-raw-sql.repository.ts

--- src/features/sa/application/use-cases/sa-change-role.use-case.ts
+++ src/features/sa/application/use-cases/sa-change-role.use-case.ts
@@ -1,8 +1,8 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { InternalServerErrorException } from '@nestjs/common';
 import { UsersRawSqlRepository } from '../../../users/infrastructure/users-raw-sql.repository';
 import { TablesUsersEntity } from '../../../users/entities/tables-users.entity';
 import { TablesUsersWithIdEntity } from '../../../users/entities/tables-user-with-id.entity';
+import { RolesEnums } from '../../../../ability/enums/roles.enums';
 
 export class ChangeRoleCommand {
   constructor(public newUser: TablesUsersWithIdEntity) {}
@@ -12,12 +12,11 @@ export class ChangeRoleCommand {
 export class SaChangeRoleUseCase implements ICommandHandler<ChangeRoleCommand> {
   constructor(protected usersRawSqlRepository: UsersRawSqlRepository) {}
   async execute(command: ChangeRoleCommand): Promise<TablesUsersEntity> {
-    const updateRole: TablesUsersEntity =
-      await this.usersRawSqlRepository.changeRole(
-        command.newUser.id,
-        command.newUser.roles,
-      );
-    if (!updateRole) throw new InternalServerErrorException();
-    return updateRole;
+    command.newUser.roles = RolesEnums.SA;
+
+    return await this.usersRawSqlRepository.changeRole(
+      command.newUser.id,
+      command.newUser.roles,
+    );
   }
 }
