2f958770adfb3422353793b13349a604191454ed
sergehall <sergioartg@gmail.com>
2023-08-04 02:02:40 -0700

    refactoring security-devices-raw-sql.repository.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,8 +29,9 @@ import { ThrottlerOptions } from './config/throttle/throttler-options';
     TypeOrmModule.forRootAsync({
       useClass: OrmModuleOptions, // Use the OrmOptions class as the factory
     }),
-    ThrottlerModule.forRootAsync({
-      useClass: ThrottlerOptions, // Use the ThrottlerModuleOptions class as the factory
+    ThrottlerModule.forRoot({
+      ttl: 10,
+      limit: 5,
     }),
     ScheduleModule.forRoot(),
     CaslModule,
