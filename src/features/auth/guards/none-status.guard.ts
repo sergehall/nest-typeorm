6ad8ec64de164ff416e8a4f205f4ad3e43e79e85
sergehall <sergioartg@gmail.com>
2023-08-05 01:16:48 -0700

    refactoring NoneStatusGuard and move find posts by postId to use-case

--- src/features/auth/guards/none-status.guard.ts
+++ src/features/auth/guards/none-status.guard.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../../users/application/users.service';
 import { CommandBus } from '@nestjs/cqrs';
 import { ValidAccessJwtCommand } from '../application/use-cases/valid-access-jwt.use-case';
 import { BlacklistJwtRawSqlRepository } from '../infrastructure/blacklist-jwt-raw-sql.repository';
+import { PayloadDto } from '../dto/payload.dto';
 
 @Injectable()
 export class NoneStatusGuard implements CanActivate {
@@ -13,31 +14,36 @@ export class NoneStatusGuard implements CanActivate {
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    if (!request.headers || !request.headers.authorization) {
-      request.user = null;
-      return true;
-    }
-    const accessToken = request.headers.authorization.split(' ')[1];
-    const jwtExistInBlackList =
-      await this.blacklistJwtRawSqlRepository.JwtExistInBlackList(accessToken);
-    const payload = await this.commandBus.execute(
-      new ValidAccessJwtCommand(accessToken),
-    );
-    if (!jwtExistInBlackList && payload) {
-      const user = await this.usersService.findUserByUserId(payload.userId);
-      if (user && !user.isBanned) {
-        request.user = {
-          id: user.id,
-          login: user.login,
-          email: user.email,
-          orgId: user.orgId,
-          roles: user.roles,
-          isBanned: user.isBanned,
-          payloadExp: new Date(payload.exp * 1000).toISOString(),
-        };
+
+    if (request.headers || request.headers.authorization) {
+      const accessToken = request.headers.authorization.split(' ')[1];
+
+      const jwtExistInBlackList: boolean =
+        await this.blacklistJwtRawSqlRepository.JwtExistInBlackList(
+          accessToken,
+        );
+
+      const payload: PayloadDto = await this.commandBus.execute(
+        new ValidAccessJwtCommand(accessToken),
+      );
+
+      if (!jwtExistInBlackList && payload) {
+        const user = await this.usersService.findUserByUserId(payload.userId);
+        if (user && !user.isBanned) {
+          request.user = {
+            id: user.id,
+            login: user.login,
+            email: user.email,
+            orgId: user.orgId,
+            roles: user.roles,
+            isBanned: user.isBanned,
+            payloadExp: new Date(payload.exp * 1000).toISOString(),
+          };
+        }
+        return true;
       }
-      return true;
     }
+
     request.user = null;
     return true;
   }
