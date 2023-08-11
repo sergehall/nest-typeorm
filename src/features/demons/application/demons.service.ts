7728129db3d88878dbbc05f6a73dd3696aeecd1c
sergehall <sergioartg@gmail.com>
2023-08-11 03:01:01 -0700

    refactoring mails.module.ts

--- src/features/demons/application/demons.service.ts
+++ src/features/demons/application/demons.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { MailsService } from '../../mails/application/mails.service';
 import { CommandBus } from '@nestjs/cqrs';
 import { BlacklistJwtRawSqlRepository } from '../../auth/infrastructure/blacklist-jwt-raw-sql.repository';
 import { SecurityDevicesRawSqlRepository } from '../../security-devices/infrastructure/security-devices-raw-sql.repository';
 import { UsersRawSqlRepository } from '../../users/infrastructure/users-raw-sql.repository';
 import { RemoveDataUsersWithExpiredDateCommand } from './use-case/remove-data-users-with-expired-date.use-case';
 import { MailsRawSqlRepository } from '../../mails/infrastructure/mails-raw-sql.repository';
-import { FindAndSendConfirmationCommand } from '../../mails/application/use-cases/find-and-send-confirmation-code.use-case';
-import { FindAndSendRecoveryCodeCommand } from '../../mails/application/use-cases/find-and-send-recovery-code.use-case';
+import { FindAndSendConfirmationCommand } from '../../mails/application/use-case/find-and-send-confirmation-code.use-case';
+import { FindAndSendRecoveryCodeCommand } from '../../mails/application/use-case/find-and-send-recovery-code.use-case';
 
 @Injectable()
 export class DemonsService {
   constructor(
-    protected mailsService: MailsService,
     protected blacklistJwtRawSqlRepository: BlacklistJwtRawSqlRepository,
     protected securityDevicesRawSqlRepository: SecurityDevicesRawSqlRepository,
     protected usersRawSqlRepository: UsersRawSqlRepository,
@@ -45,15 +43,15 @@ export class DemonsService {
     await this.commandBus.execute(new RemoveDataUsersWithExpiredDateCommand());
   }
 
+  // every 30 min
+  @Cron('*/30 * * * *')
+  async clearingSentEmails() {
+    await this.mailsRawSqlRepository.clearingSentEmails();
+  }
+
   // every 1 hour
   @Cron('0 * * * *')
   async clearingDevicesWithExpiredDate() {
     await this.securityDevicesRawSqlRepository.clearingDevicesWithExpiredDate();
-  }
-
-  // // // every 1 hour
-  @Cron('0 * * * *')
-  async clearingSentEmails() {
-    await this.mailsRawSqlRepository.clearingSentEmails();
   }
 }
