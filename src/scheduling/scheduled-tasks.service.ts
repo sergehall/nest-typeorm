2177ffd8e0bb831f2702a2345eb4b97459f232ee
sergehall <sergioartg@gmail.com>
2023-08-11 23:21:23 -0700

    rename demon to scheduling.module

--- src/scheduling/scheduled-tasks.service.ts
+++ src/scheduling/scheduled-tasks.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { CommandBus } from '@nestjs/cqrs';
-import { BlacklistJwtRawSqlRepository } from '../../auth/infrastructure/blacklist-jwt-raw-sql.repository';
-import { SecurityDevicesRawSqlRepository } from '../../security-devices/infrastructure/security-devices-raw-sql.repository';
-import { UsersRawSqlRepository } from '../../users/infrastructure/users-raw-sql.repository';
-import { RemoveDataUsersWithExpiredDateCommand } from './use-case/remove-data-users-with-expired-date.use-case';
-import { MailsRawSqlRepository } from '../../mails/infrastructure/mails-raw-sql.repository';
-import { FindAndSendConfirmationCommand } from '../../mails/application/use-case/find-and-send-confirmation-code.use-case';
-import { FindAndSendRecoveryCodeCommand } from '../../mails/application/use-case/find-and-send-recovery-code.use-case';
+import { BlacklistJwtRawSqlRepository } from '../features/auth/infrastructure/blacklist-jwt-raw-sql.repository';
+import { SecurityDevicesRawSqlRepository } from '../features/security-devices/infrastructure/security-devices-raw-sql.repository';
+import { UsersRawSqlRepository } from '../features/users/infrastructure/users-raw-sql.repository';
+import { RemoveDataUsersWithExpiredDateCommand } from '../features/users/application/use-cases/remove-data-users-with-expired-date.use-case';
+import { MailsRawSqlRepository } from '../features/mails/infrastructure/mails-raw-sql.repository';
+import { FindAndSendConfirmationCommand } from '../features/mails/application/use-case/find-and-send-confirmation-code.use-case';
+import { FindAndSendRecoveryCodeCommand } from '../features/mails/application/use-case/find-and-send-recovery-code.use-case';
 
 @Injectable()
-export class DemonsService {
+export class ScheduledTasksService {
   constructor(
     protected blacklistJwtRawSqlRepository: BlacklistJwtRawSqlRepository,
     protected securityDevicesRawSqlRepository: SecurityDevicesRawSqlRepository,
