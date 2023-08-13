7688a4ea35fa313dfce43f423d25085cae413e24
sergehall <sergioartg@gmail.com>
2023-08-13 02:23:08 -0700

    refactoring MailsModule

--- src/mails/application/types/sent-email-table-names.ts
+++
@@ -1,3 +0,0 @@
-export type SentEmailTableNames =
-  | 'EmailsConfirmationCodes'
-  | 'EmailsRecoveryCodes';
--- src/scheduling/scheduling.module.ts
+++ src/scheduling/scheduling.module.ts
@@ -18,7 +18,7 @@ import { PostgresConfig } from '../config/db/postgres/postgres.config';
 import { KeyArrayProcessor } from '../common/query/get-key-from-array-or-default';
 import { DataCleanupService } from '../data-cleanup/data-cleanup.service';
 import { SentCodeLogRepository } from '../mails/infrastructure/sent-code-log.repository';
-import { MailOptionsBuilder } from '../mails/adapters/mail-options-builder';
+import { MailOptionsBuilder } from '../mails/mail-options/mail-options-builder';
 
 @Module({
   imports: [MailsModule, CaslModule, CqrsModule],
