6fc46340505a915dd2e0c10c537e9114ded5b2f7
sergehall <sergioartg@gmail.com>
2023-08-13 18:21:35 -0700

    export EmailNotExistValidator

--- src/features/auth/dto/email.dto.ts
+++ src/features/auth/dto/email.dto.ts
@@ -1,7 +1,9 @@
-import { IsNotEmpty, Matches } from 'class-validator';
+import { IsNotEmpty, Matches, Validate } from 'class-validator';
+import { EmailNotExistValidator } from '../../../common/validators/email-not-exist.validator';
 
 export class EmailDto {
   @IsNotEmpty()
   @Matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')
+  @Validate(EmailNotExistValidator)
   email: string;
 }
