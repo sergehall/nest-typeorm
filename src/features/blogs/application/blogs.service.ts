a772ee4ed1bc03d4487c97e2ffd848a28024f694
sergehall <sergioartg@gmail.com>
2023-08-15 21:45:21 -0700

    add GetBlogByIdUseCase

--- src/features/blogs/application/blogs.service.ts
+++ src/features/blogs/application/blogs.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { BloggerBlogsService } from '../../blogger-blogs/application/blogger-blogs.service';
-import { ReturnBloggerBlogsEntity } from '../../blogger-blogs/entities/return-blogger-blogs.entity';
 
 @Injectable()
-export class BlogsService {
-  constructor(protected bloggerBlogsService: BloggerBlogsService) {}
-  async openFindBlogById(blogId: string): Promise<ReturnBloggerBlogsEntity> {
-    return await this.bloggerBlogsService.openFindBlogById(blogId);
-  }
-}
+export class BlogsService {}
---
+++ src/features/blogs/application/use-cases/get-blog-by-id.use-case.ts
@@ -0,0 +1,36 @@
+import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BloggerBlogsRawSqlRepository } from '../../../blogger-blogs/infrastructure/blogger-blogs-raw-sql.repository';
+import { ReturnBloggerBlogsEntity } from '../../../blogger-blogs/entities/return-blogger-blogs.entity';
+import { NotFoundException } from '@nestjs/common';
+
+export class GetBlogByIdCommand {
+  constructor(public blogId: string) {}
+}
+
+@CommandHandler(GetBlogByIdCommand)
+export class GetBlogByIdUseCase implements ICommandHandler<GetBlogByIdCommand> {
+  constructor(
+    protected bloggerBlogsRawSqlRepository: BloggerBlogsRawSqlRepository,
+    protected commandBus: CommandBus,
+  ) {}
+  async execute(
+    command: GetBlogByIdCommand,
+  ): Promise<ReturnBloggerBlogsEntity> {
+    const { blogId } = command;
+
+    const blog = await this.bloggerBlogsRawSqlRepository.findBlogById(blogId);
+
+    if (!blog) {
+      throw new NotFoundException('Blog not found');
+    }
+
+    return {
+      id: blog.id,
+      name: blog.name,
+      description: blog.description,
+      websiteUrl: blog.websiteUrl,
+      createdAt: blog.createdAt,
+      isMembership: blog.isMembership,
+    };
+  }
+}
