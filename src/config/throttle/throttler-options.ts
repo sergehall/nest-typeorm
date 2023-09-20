b77e947c988c5f70534608d7a838848735860a14
sergehall <sergioartg@gmail.com>
2023-09-19 19:02:10 -0700

    rolling back the update

--- src/config/throttle/throttler-options.ts
+++ src/config/throttle/throttler-options.ts
@@ -13,6 +13,6 @@ export class ThrottlerOptions
   async createThrottlerOptions(): Promise<ThrottlerModuleOptions> {
     const ttl: number = await this.getValueThrottle('THROTTLE_TTL');
     const limit: number = await this.getValueThrottle('THROTTLE_LIMIT');
-    return { throttlers: [{ ttl: ttl, limit: limit }] };
+    return { ttl: ttl, limit: limit };
   }
 }
