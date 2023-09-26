eb20686a0d5874a3fb9c8d13f517572dd5ed8d49
sergehall <sergioartg@gmail.com>
2023-09-25 22:19:46 -0700

    add answer-to-current-question.use-case.ts

--- src/features/pair-game-quiz/dto/update-pair-game-quiz.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePairGameQuizDto } from './create-pair-game-quiz.dto';
-
-export class UpdatePairGameQuizDto extends PartialType(CreatePairGameQuizDto) {}
