e376fffb87675a8af7c68191c936c792e1091687
sergehall <sergioartg@gmail.com>
2023-10-06 01:25:57 -0700

    refactoring start-game.use-case.ts

--- src/features/pair-game-quiz/api/pair-game-quiz.controller.ts
+++ src/features/pair-game-quiz/api/pair-game-quiz.controller.ts
@@ -36,6 +36,7 @@ export class PairGameQuizController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
   @Get('my-current')
   async findUnfinishedGame(@Request() req: any): Promise<GameViewModel> {
     const currentUserDto: CurrentUserDto = req.user;
