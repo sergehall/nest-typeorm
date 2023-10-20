69e7e5aa1f1e69bb874cacacd0b69c27eb3556ca
sergehall <sergioartg@gmail.com>
2023-10-20 01:05:44 -0700

    refactoring SubmitAnswerForCurrentQuestionUseCase

--- src/features/pair-game-quiz/application/use-cases/player-answers-all-questions.use-case.ts
+++ src/features/pair-game-quiz/application/use-cases/player-answers-all-questions.use-case.ts
@@ -34,16 +34,20 @@ export class PlayerAnswersAllQuestionsUseCase
   async execute(command: PlayerAnswersAllQuestionsCommand): Promise<boolean> {
     const { game, currentUserDto } = command;
     const TEN_SECONDS = 10000; // 10 seconds in milliseconds
-    // Schedule a separate asynchronous operation for saveGame after a 10-second delay
+
+    // Schedule a separate asynchronous operation to finish the game after a 10-second delay
     setTimeout(async () => {
-      // After the 10-second delay, update the game status StatusGameEnum.FINISHED and add finishGameDate
+      // After the 10-second delay, update the game status to "FINISHED" and record the finishGameDate
       game.status = StatusGameEnum.FINISHED;
       game.finishGameDate = new Date().toISOString();
 
+      // Save the updated game status
       await this.gamePairsRepo.saveGame(game);
 
+      // Handle unanswered questions for the other player
       await this.finishForAnotherUser(game, currentUserDto);
 
+      // Trigger AddResultToPairGameCommand to add results to the game
       await this.commandBus.execute(new AddResultToPairGameCommand(game));
     }, TEN_SECONDS);
 
@@ -51,7 +55,7 @@ export class PlayerAnswersAllQuestionsUseCase
     return true;
   }
 
-  // New method to check unanswered questions by the second user.
+  // Method to handle unanswered questions by the other player.
   private async finishForAnotherUser(
     game: PairsGameEntity,
     currentUserDto: CurrentUserDto,
@@ -61,14 +65,17 @@ export class PlayerAnswersAllQuestionsUseCase
         ? game.secondPlayer!.userId
         : game.firstPlayer.userId;
 
+    // Retrieve challenge answers associated with the game for the other player
     const challengesAnswers: ChallengeAnswersEntity[] =
       await this.challengesAnswersRepo.getCountChallengeAnswersByGameIdUserId(
         game.id,
         anotherUserId,
       );
 
+    // Calculate the number of challenge answers
     const countChallengeAnswers = challengesAnswers.length;
 
+    // Retrieve the remaining challenge questions that the other player has not answered
     const remainingQuestions: ChallengeQuestionsEntity[] =
       await this.challengesQuestionsRepo.getRemainingChallengeQuestions(
         game.id,
@@ -77,6 +84,7 @@ export class PlayerAnswersAllQuestionsUseCase
 
     const challengeAnswers: ChallengeAnswersEntity[] = [];
 
+    // Create answers arr  with a predefined text for remaining unanswered questions
     if (remainingQuestions && remainingQuestions.length > 0) {
       for (const challengeQuestion of remainingQuestions) {
         const answerOwnerEntity = new UsersEntity();
@@ -101,8 +109,10 @@ export class PlayerAnswersAllQuestionsUseCase
       }
     } else {
       console.log('No remaining questions to answer.');
+      return true;
     }
 
+    // Save the constructed challenge answers
     await this.challengesAnswersRepo.saveChallengeAnswersEntities(
       challengeAnswers,
     );
