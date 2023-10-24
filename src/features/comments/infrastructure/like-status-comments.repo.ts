2878911f0d3de4024da9a7ab74668307ef5d9000
sergehall <sergioartg@gmail.com>
2023-10-24 00:56:40 -0700

    refactoring like-status-comments.entity.ts and like-status-posts.entity.ts

--- src/features/comments/infrastructure/like-status-comments.repo.ts
+++ src/features/comments/infrastructure/like-status-comments.repo.ts
@@ -3,12 +3,8 @@ import { Repository } from 'typeorm';
 import { LikeStatusCommentsEntity } from '../entities/like-status-comments.entity';
 import { LikeStatusDto } from '../dto/like-status.dto';
 import { CommentsEntity } from '../entities/comments.entity';
-import { UsersEntity } from '../../users/entities/users.entity';
-import { BloggerBlogsEntity } from '../../blogger-blogs/entities/blogger-blogs.entity';
-import * as uuid4 from 'uuid4';
 import { CurrentUserDto } from '../../users/dto/current-user.dto';
 import { InternalServerErrorException } from '@nestjs/common';
-import { PostsEntity } from '../../posts/entities/posts.entity';
 import { LikesDislikesMyStatusInfoDto } from '../dto/likes-dislikes-my-status-info.dto';
 import { LikeStatusEnums } from '../../../db/enums/like-status.enums';
 
@@ -50,7 +46,7 @@ export class LikeStatusCommentsRepo {
     }
   }
 
-  async updateLikeStatusComment(
+  async updateOrCreateLikeStatusComment(
     comment: CommentsEntity,
     likeStatusDto: LikeStatusDto,
     currentUserDto: CurrentUserDto,
@@ -68,11 +64,12 @@ export class LikeStatusCommentsRepo {
         likeStatusCommentEntity.likeStatus = likeStatusDto.likeStatus;
         likeStatusCommentEntity.addedAt = new Date().toISOString();
       } else {
-        likeStatusCommentEntity = await this.createLikeStatusCommentsEntity(
-          comment,
-          likeStatusDto,
-          currentUserDto,
-        );
+        likeStatusCommentEntity =
+          LikeStatusCommentsEntity.createLikeStatusCommentsEntity(
+            comment,
+            likeStatusDto,
+            currentUserDto,
+          );
       }
 
       return await this.likeStatusCommentRepository.save(
@@ -82,40 +79,5 @@ export class LikeStatusCommentsRepo {
       console.log(error.message);
       throw new InternalServerErrorException(error.message);
     }
-  }
-
-  private async createLikeStatusCommentsEntity(
-    findComment: CommentsEntity,
-    likeStatusDto: LikeStatusDto,
-    currentUserDto: CurrentUserDto,
-  ): Promise<LikeStatusCommentsEntity> {
-    const blogEntity = new BloggerBlogsEntity();
-    blogEntity.id = findComment.blog.id;
-
-    const postEntity = new PostsEntity();
-    postEntity.id = findComment.post.id;
-
-    const ownerUserEntity = new UsersEntity();
-    ownerUserEntity.userId = findComment.commentator.userId;
-
-    const ratedUserEntity = new UsersEntity();
-    ratedUserEntity.userId = currentUserDto.userId;
-    ratedUserEntity.login = currentUserDto.login;
-    ratedUserEntity.isBanned = currentUserDto.isBanned;
-
-    const commentEntity = new CommentsEntity();
-    commentEntity.id = findComment.id;
-
-    return {
-      id: uuid4().toString(),
-      likeStatus: likeStatusDto.likeStatus,
-      addedAt: new Date().toISOString(),
-      isBanned: false,
-      comment: commentEntity,
-      ratedCommentUser: ratedUserEntity,
-      blog: blogEntity,
-      post: postEntity,
-      commentOwner: ownerUserEntity,
-    };
   }
 }
--- src/features/posts/infrastructure/like-status-posts.repo.ts
+++ src/features/posts/infrastructure/like-status-posts.repo.ts
@@ -1,9 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { LikeStatusDto } from '../dto/like-status.dto';
-import { UsersEntity } from '../../users/entities/users.entity';
-import { BloggerBlogsEntity } from '../../blogger-blogs/entities/blogger-blogs.entity';
-import * as uuid4 from 'uuid4';
 import { CurrentUserDto } from '../../users/dto/current-user.dto';
 import { InternalServerErrorException } from '@nestjs/common';
 import { LikeStatusPostsEntity } from '../entities/like-status-posts.entity';
@@ -32,11 +29,12 @@ export class LikeStatusPostsRepo {
         likeStatusPostEntity.likeStatus = likeStatusDto.likeStatus;
         likeStatusPostEntity.addedAt = new Date().toISOString();
       } else {
-        likeStatusPostEntity = await this.likeStatusPostsEntity(
-          post,
-          likeStatusDto,
-          currentUserDto,
-        );
+        likeStatusPostEntity =
+          LikeStatusPostsEntity.createLikeStatusPostsEntity(
+            post,
+            likeStatusDto,
+            currentUserDto,
+          );
       }
 
       return await this.likeStatusPostsRepository.save(likeStatusPostEntity);
@@ -44,33 +42,5 @@ export class LikeStatusPostsRepo {
       console.log(error.message);
       throw new InternalServerErrorException(error.message);
     }
-  }
-
-  private async likeStatusPostsEntity(
-    post: PostsEntity,
-    likeStatusDto: LikeStatusDto,
-    currentUserDto: CurrentUserDto,
-  ): Promise<LikeStatusPostsEntity> {
-    const blogEntity = new BloggerBlogsEntity();
-    blogEntity.id = post.blog.id;
-
-    const postOwnerEntity = new UsersEntity();
-    postOwnerEntity.userId = post.postOwner.userId;
-
-    const ratedPostUserEntity = new UsersEntity();
-    ratedPostUserEntity.userId = currentUserDto.userId;
-    ratedPostUserEntity.login = currentUserDto.login;
-    ratedPostUserEntity.isBanned = currentUserDto.isBanned;
-
-    return {
-      id: uuid4().toString(),
-      likeStatus: likeStatusDto.likeStatus,
-      addedAt: new Date().toISOString(),
-      isBanned: false,
-      blog: blogEntity,
-      post: post,
-      postOwner: postOwnerEntity,
-      ratedPostUser: ratedPostUserEntity,
-    };
   }
 }
