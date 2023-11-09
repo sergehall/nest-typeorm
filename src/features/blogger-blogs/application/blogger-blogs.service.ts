ae18fe49e24614373d9589a7b6c48f0510a01947
sergehall <sergioartg@gmail.com>
2023-11-08 22:34:26 -0800

    test 32 Done

--- src/features/blogger-blogs/application/blogger-blogs.service.ts
+++ src/features/blogger-blogs/application/blogger-blogs.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { BloggerBlogsEntity } from '../entities/blogger-blogs.entity';
 import { BloggerBlogsViewModel } from '../view-models/blogger-blogs.view-model';
-import { SaBloggerBlogsViewModel } from '../../sa/view-models/sa-blogger-blogs.view-model';
 
 @Injectable()
 export class BloggerBlogsService {
@@ -15,27 +14,6 @@ export class BloggerBlogsService {
       websiteUrl: row.websiteUrl,
       createdAt: row.createdAt,
       isMembership: row.isMembership,
-    }));
-  }
-
-  async transformBlogsForSa(
-    blogs: BloggerBlogsEntity[],
-  ): Promise<SaBloggerBlogsViewModel[]> {
-    return blogs.map((blog: BloggerBlogsEntity) => ({
-      id: blog.id,
-      name: blog.name,
-      description: blog.description,
-      websiteUrl: blog.websiteUrl,
-      createdAt: blog.createdAt,
-      isMembership: blog.isMembership,
-      blogOwnerInfo: {
-        userId: blog.blogOwner.userId,
-        userLogin: blog.blogOwner.login,
-      },
-      banInfo: {
-        isBanned: blog.isBanned,
-        banDate: blog.banDate,
-      },
     }));
   }
 }
--- src/features/sa/application/sa.service.ts
+++ src/features/sa/application/sa.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { BloggerBlogsEntity } from '../../blogger-blogs/entities/blogger-blogs.entity';
+import { SaBloggerBlogsViewModel } from '../view-models/sa-blogger-blogs.view-model';
 
 @Injectable()
-export class SaService {}
+export class SaService {
+  async transformBlogsForSa(
+    blogs: BloggerBlogsEntity[],
+  ): Promise<SaBloggerBlogsViewModel[]> {
+    return blogs.map((blog: BloggerBlogsEntity) => ({
+      id: blog.id,
+      name: blog.name,
+      description: blog.description,
+      websiteUrl: blog.websiteUrl,
+      createdAt: blog.createdAt,
+      isMembership: blog.isMembership,
+      blogOwnerInfo: {
+        userId: blog.blogOwner.userId,
+        userLogin: blog.blogOwner.login,
+      },
+      banInfo: {
+        isBanned: blog.isBanned,
+        banDate: blog.banDate,
+      },
+    }));
+  }
+}
