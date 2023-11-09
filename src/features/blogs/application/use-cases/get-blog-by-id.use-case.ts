10c6ead363ca043bc577a4b09d35a106e8299cbe
sergehall <sergioartg@gmail.com>
2023-11-08 21:08:24 -0800

    update findBlogById

--- src/features/blogs/application/use-cases/get-blog-by-id.use-case.ts
+++ src/features/blogs/application/use-cases/get-blog-by-id.use-case.ts
@@ -2,6 +2,7 @@ import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { NotFoundException } from '@nestjs/common';
 import { BloggerBlogsRepo } from '../../../blogger-blogs/infrastructure/blogger-blogs.repo';
 import { BloggerBlogsViewModel } from '../../../blogger-blogs/view-models/blogger-blogs.view-model';
+import { BloggerBlogsEntity } from '../../../blogger-blogs/entities/blogger-blogs.entity';
 
 export class GetBlogByIdCommand {
   constructor(public blogId: string) {}
@@ -16,7 +17,8 @@ export class GetBlogByIdUseCase implements ICommandHandler<GetBlogByIdCommand> {
   async execute(command: GetBlogByIdCommand): Promise<BloggerBlogsViewModel> {
     const { blogId } = command;
 
-    const blog = await this.bloggerBlogsRepo.findBlogById(blogId);
+    const blog: BloggerBlogsEntity | null =
+      await this.bloggerBlogsRepo.findBlogById(blogId);
 
     if (!blog) {
       throw new NotFoundException(`Blog with id: ${blogId} not found`);
