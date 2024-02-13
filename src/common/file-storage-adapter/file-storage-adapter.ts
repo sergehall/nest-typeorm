4be58a74612de99fd04faec9f720810afd538bfd
sergehall <sergioartg@gmail.com>
2024-02-13 03:12:26 -0800

    refactoring FileStorageAdapter

--- src/common/file-storage-adapter/file-storage-adapter.ts
+++ src/common/file-storage-adapter/file-storage-adapter.ts
@@ -11,67 +11,37 @@ import { BlogIdParams } from '../query/params/blogId.params';
 export class FileStorageAdapter {
   constructor(private s3Service: S3Service) {}
 
-  /**
-   * Uploads a file to AWS S3 for a specific blog post.
-   * @param params Parameters identifying the blog post.
-   * @param fileUploadDto Information about the file to upload.
-   * @param currentUserDto Information about the current user.
-   * @returns Information about the uploaded object.
-   */
-  async uploadFileForPost(
+  async uploadFileImagePost(
     params: BlogIdPostIdParams,
     fileUploadDto: FileUploadDtoDto,
     currentUserDto: CurrentUserDto,
   ): Promise<UrlEtagDto> {
     const { blogId, postId } = params;
-    const { buffer, mimetype } = fileUploadDto;
-    const fileExtension = await this.getFileExtension(mimetype);
-    const s3Client = await this.s3Service.getS3Client();
-    const bucketName = await this.s3Service.getS3BucketName();
-    const key = `content/users/${currentUserDto.userId}/blogs/${blogId}/posts/${postId}_post.${fileExtension}`;
-
-    const bucketParams = {
-      Bucket: bucketName,
-      Key: key,
-      Body: buffer,
-      ContentType: mimetype,
-    };
-
-    const command: PutObjectCommand = new PutObjectCommand(bucketParams);
-    const resultUploaded: PutObjectCommandOutput = await s3Client.send(command);
-    const eTag = resultUploaded.ETag;
-    if (!eTag) {
-      console.error('Error uploading file to S3:');
-      throw new InternalServerErrorException('Error uploading file to S3:');
-    }
-    try {
-      return { url: key, eTag: eTag };
-    } catch (error) {
-      console.error('Error uploading file to S3:', error);
-      throw new InternalServerErrorException(
-        'Error uploading file to S3:' + error.message,
-      );
-    }
+    const key = this.generateKeyForPostFile(
+      currentUserDto.userId,
+      blogId,
+      postId,
+    );
+    return this.uploadFile(key, fileUploadDto);
   }
 
-  /**
-   * Uploads a file to AWS S3 for a specific blog post.
-   * @param params Parameters identifying the blog post.
-   * @param fileUploadDto Information about the file to upload.
-   * @param currentUserDto Information about the current user.
-   * @returns Information about the uploaded object.
-   */
   async uploadFileImageBlogWallpaper(
     params: BlogIdParams,
     fileUploadDto: FileUploadDtoDto,
     currentUserDto: CurrentUserDto,
   ): Promise<UrlEtagDto> {
     const { blogId } = params;
+    const key = this.generateKeyForWallpaperFile(currentUserDto.userId, blogId);
+    return this.uploadFile(key, fileUploadDto);
+  }
+
+  private async uploadFile(
+    key: string,
+    fileUploadDto: FileUploadDtoDto,
+  ): Promise<UrlEtagDto> {
     const { buffer, mimetype } = fileUploadDto;
-    const fileExtension = await this.getFileExtension(mimetype);
     const s3Client = await this.s3Service.getS3Client();
     const bucketName = await this.s3Service.getS3BucketName();
-    const key = `content/users/${currentUserDto.userId}/blogs/${blogId}_wallpaper.${fileExtension}`;
 
     const bucketParams = {
       Bucket: bucketName,
@@ -83,10 +53,12 @@ export class FileStorageAdapter {
     const command: PutObjectCommand = new PutObjectCommand(bucketParams);
     const resultUploaded: PutObjectCommandOutput = await s3Client.send(command);
     const eTag = resultUploaded.ETag;
+
     if (!eTag) {
       console.error('Error uploading file to S3:');
       throw new InternalServerErrorException('Error uploading file to S3:');
     }
+
     try {
       return { url: key, eTag: eTag };
     } catch (error) {
@@ -97,13 +69,23 @@ export class FileStorageAdapter {
     }
   }
 
-  /**
-   * Generates a unique key for the uploaded file based on its original name.
-   * @param originalname Original name of the uploaded file.
-   * @returns Unique key for the file.
-   */
-  private async getFileExtension(originalname: string): Promise<string> {
-    const parts = originalname.split('/');
-    return parts.length > 1 ? parts[parts.length - 1] : '';
+  private generateKeyForPostFile(
+    userId: string,
+    blogId: string,
+    postId: string,
+  ): string {
+    return `content/users/${userId}/blogs/${blogId}/posts/${postId}_post.${this.getFileExtension(
+      postId,
+    )}`;
+  }
+
+  private generateKeyForWallpaperFile(userId: string, blogId: string): string {
+    return `content/users/${userId}/blogs/${blogId}_wallpaper.${this.getFileExtension(
+      blogId,
+    )}`;
+  }
+
+  private getFileExtension(filename: string): string {
+    return filename.split('.').pop() || '';
   }
 }
