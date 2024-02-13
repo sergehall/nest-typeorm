9cc8105c02ce63d5c2ae2e0384699561b2bc483e
sergehall <sergioartg@gmail.com>
2024-02-12 23:31:21 -0800

    refactoring testing-delete-all-data.repository.ts

--- src/features/testing/infrastructure/testing-delete-all-data.repository.ts
+++ src/features/testing/infrastructure/testing-delete-all-data.repository.ts
@@ -12,6 +12,7 @@ export class TestingDeleteAllDataRepository {
 
   async removeAllData(): Promise<void> {
     const tablesToDelete = [
+      'PostsImagesFileMetadata',
       'ChallengeAnswers',
       'ChallengeQuestions',
       'PairsGame',
