92cd1ca58832e6858ce498c1266dba9d3feaa23d
sergehall <sergioartg@gmail.com>
2024-02-29 22:35:38 -0800

    refactoring TelegramTextParserUseCase

--- src/features/telegram/application/use-cases/telegram-text-parser.use-case.ts
+++ src/features/telegram/application/use-cases/telegram-text-parser.use-case.ts
@@ -11,9 +11,9 @@ export class TelegramTextParserUseCase
 {
   constructor() {}
 
-  async execute(command: TelegramTextParserCommand): Promise<string> {
-    const { payloadTelegramMessage } = command;
-
+  async execute({
+    payloadTelegramMessage,
+  }: TelegramTextParserCommand): Promise<string> {
     const nameRecipient =
       payloadTelegramMessage.message.from.first_name ||
       payloadTelegramMessage.message.from.username;
@@ -25,41 +25,59 @@ export class TelegramTextParserUseCase
     const words: string[] = text.toLowerCase().match(/\w+|[^\w\s]/g) || [];
 
     // Check if the input contains the word "hello"
-    if (words.length != 0 && words.includes('hello')) {
+    if (words.length !== 0 && words.includes('hello')) {
       return `Hello, ${nameRecipient}! How are you today?`;
     }
 
     // Analyze the other words and create a response
-    let response = '';
+    let response: string = '';
+    const incomprehensibleWords: string[] = [];
     for (const word of words) {
       if (!commonWords.includes(word)) {
+        incomprehensibleWords.push(word);
         switch (word) {
           case 'goodbye':
-            response += `Goodbye ${nameRecipient}! Have a great day! `;
+            response += `Goodbye, ${nameRecipient}! Have a great day! `;
             break;
           case 'help':
-            response += "Sure, I'm here to assist you! ";
+            response += `Sure, I'm here to assist you! `;
             break;
           case 'weather':
             response += 'The weather is sunny and warm today. ';
             break;
           case 'time':
-            const time = new Date().toISOString();
+            const time = new Date().toTimeString();
             response += `The current ${time}. `;
             break;
           case 'mood':
-            response += "I'm feeling great today, thank you for asking! ";
+            response += `I'm feeling great today, thank you for asking! `;
             break;
           default:
             // Check if the word is not punctuation
             if (word.match(/\w/)) {
-              response += `I'm not sure what you mean by "${word}", ${nameRecipient}. `;
+              response += `I'm not sure what you mean by "${incomprehensibleWords}", ${nameRecipient}. `;
             }
             break;
         }
       }
     }
+    const answer = await this.generateResponse(
+      response,
+      incomprehensibleWords,
+      nameRecipient,
+    );
 
+    return answer;
+  }
+
+  private async generateResponse(
+    response: string,
+    incomprehensibleWords: string[],
+    nameRecipient: string,
+  ): Promise<string> {
+    for (const word of incomprehensibleWords) {
+      response += `I'm not sure what you mean by "${word}", ${nameRecipient}. `;
+    }
     return response.trim();
   }
 }
