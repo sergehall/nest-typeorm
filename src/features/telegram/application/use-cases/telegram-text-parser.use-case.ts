3547934f364e1f44fdd2ef3bda80e7836e3b60d0
sergehall <sergioartg@gmail.com>
2024-03-01 21:37:27 -0800

    refactoring TelegramTextParserUseCase

--- src/features/telegram/application/use-cases/telegram-text-parser.use-case.ts
+++ src/features/telegram/application/use-cases/telegram-text-parser.use-case.ts
@@ -1,7 +1,7 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PayloadTelegramMessageType } from '../../types/payload-telegram-message.type';
-import { dialogsSets } from '../../helpers/dialogs-sets';
-import { LevenshteinDistance } from '../../helpers/levenshtein-distance';
+import * as similarity from 'similarity';
+import { dialogsSets, DialogTemplate } from '../../helpers/dialogs-sets';
 
 export class TelegramTextParserCommand {
   constructor(public payloadTelegramMessage: PayloadTelegramMessageType) {}
@@ -11,122 +11,33 @@ export class TelegramTextParserCommand {
 export class TelegramTextParserUseCase
   implements ICommandHandler<TelegramTextParserCommand>
 {
-  private similarityThreshold = 0.7; // Adjust similarity threshold as needed
-
-  constructor(private readonly levenshteinDistance: LevenshteinDistance) {}
-
   async execute({
     payloadTelegramMessage,
   }: TelegramTextParserCommand): Promise<string> {
-    const text = payloadTelegramMessage.message.text.toLowerCase();
+    const inputPhrase = payloadTelegramMessage.message.text.toLowerCase();
     const nameRecipient =
       payloadTelegramMessage.message.from.first_name ||
       payloadTelegramMessage.message.from.username;
 
-    const response = await this.processString(text);
-    if (response) {
-      return response.replace('{nameRecipient}', nameRecipient);
+    let bestMatch: DialogTemplate | undefined;
+    let highestSimilarity = -1; // Initialize to a lower value
+
+    dialogsSets.forEach((template) => {
+      template.variations.forEach((variation) => {
+        const similarityScore = similarity(variation, inputPhrase);
+        if (similarityScore > highestSimilarity) {
+          bestMatch = template;
+          highestSimilarity = similarityScore;
+        }
+      });
+    });
+
+    if (bestMatch && highestSimilarity >= 0.9) {
+      console.log({ id: bestMatch.id, response: bestMatch.response });
+      return bestMatch.response;
     } else {
+      // Handle the case where no match is found
       return `I'm not sure what you mean, ${nameRecipient}.`;
     }
   }
-
-  private async processString(text: string): Promise<string | undefined> {
-    let maxSimilarity = 0;
-    let bestResponse: string | undefined;
-
-    for (const [phrases, response] of dialogsSets) {
-      for (const phrase of phrases) {
-        if (text.includes(phrase.toLowerCase())) {
-          console.log(text, 'text');
-          console.log(phrase, 'phrase');
-          const similarity = await this.levenshteinDistance.calculate(
-            text,
-            phrase.toLowerCase(),
-          );
-          console.log(phrase, 'phrase');
-          console.log(response, 'response');
-          if (similarity > maxSimilarity) {
-            maxSimilarity = similarity;
-            bestResponse = response;
-          }
-        }
-      }
-    }
-
-    console.log(maxSimilarity, 'maxSimilarity');
-    console.log(this.similarityThreshold, 'this.similarityThreshold');
-    console.log(bestResponse, 'bestResponse');
-
-    if (maxSimilarity >= this.similarityThreshold && bestResponse) {
-      return bestResponse;
-    }
-
-    return undefined;
-  }
 }
-
-// @CommandHandler(TelegramTextParserCommand)
-// export class TelegramTextParserUseCase
-//   implements ICommandHandler<TelegramTextParserCommand>
-// {
-//   private trie: Trie<string>;
-//   private similarityThreshold = 0.8; // Similarity threshold
-//
-//   constructor() {
-//     this.trie = DialogTrieInitializer.initializeTrie(dialogsSets);
-//   }
-//
-//   async execute({
-//     payloadTelegramMessage,
-//   }: TelegramTextParserCommand): Promise<string> {
-//     const text = payloadTelegramMessage.message.text.toLowerCase();
-//     const nameRecipient =
-//       payloadTelegramMessage.message.from.first_name ||
-//       payloadTelegramMessage.message.from.username;
-//
-//     const response = this.processString(text);
-//     if (response) {
-//       return response.replace('{nameRecipient}', nameRecipient);
-//     } else {
-//       return `I'm not sure what you mean, ${nameRecipient}.`;
-//     }
-//   }
-//
-//   private processString(text: string): string | undefined {
-//     const words = text.split(/[ ,]+/);
-//     let maxSimilarity = 0;
-//     let bestResponse: string | undefined;
-//
-//     for (const [responses, response] of dialogsSets) {
-//       for (let i = 0; i < responses.length; i++) {
-//         const phrase = responses[i].toLowerCase();
-//         const similarity = this.calculateJaccardSimilarity(
-//           words,
-//           phrase.split(/[ ,]+/),
-//         );
-//         if (similarity > maxSimilarity) {
-//           maxSimilarity = similarity;
-//           bestResponse = response;
-//         }
-//       }
-//     }
-//
-//     if (maxSimilarity >= this.similarityThreshold && bestResponse) {
-//       return bestResponse;
-//     }
-//
-//     return undefined;
-//   }
-//
-//   private calculateJaccardSimilarity(
-//     words1: string[],
-//     words2: string[],
-//   ): number {
-//     const set1 = new Set(words1);
-//     const set2 = new Set(words2);
-//     const intersection = new Set([...set1].filter((x) => set2.has(x)));
-//     const union = new Set([...set1, ...set2]);
-//     return intersection.size / union.size;
-//   }
-// }
--- src/features/telegram/helpers/dialogs-sets.ts
+++ src/features/telegram/helpers/dialogs-sets.ts
@@ -1,8 +1,18 @@
-export type DialogsSets = Array<[string[], string]>;
+export type DialogTemplate = {
+  id: string;
+  variations: string[];
+  response: string;
+};
 
-export const dialogsSets: DialogsSets = [
-  [
-    [
+export const dialogsSets: DialogTemplate[] = [
+  {
+    id: "I'm not sure what you mean",
+    variations: [],
+    response: `I'm not sure what you mean, {nameRecipient}.`,
+  },
+  {
+    id: 'greetings',
+    variations: [
       'hello',
       'hi',
       'hey',
@@ -14,10 +24,11 @@ export const dialogsSets: DialogsSets = [
       'how are you',
       'how are you?',
     ],
-    'Hello, {nameRecipient}! How are you today?',
-  ],
-  [
-    [
+    response: `Hello, {nameRecipient}! How are you today?`,
+  },
+  {
+    id: 'time',
+    variations: [
       'time',
       'current time',
       'what time is it?',
@@ -26,10 +37,11 @@ export const dialogsSets: DialogsSets = [
       "what's the time?",
       'current hour',
     ],
-    `The current ${new Date().toTimeString()}.`,
-  ],
-  [
-    [
+    response: `The current ${new Date().toTimeString()}.`,
+  },
+  {
+    id: 'mood',
+    variations: [
       'mood',
       'how are you feeling',
       "how's your mood",
@@ -37,10 +49,11 @@ export const dialogsSets: DialogsSets = [
       'feeling today',
       'emotional state',
     ],
-    `I'm feeling great today, thank you for asking!`,
-  ],
-  [
-    [
+    response: `I'm feeling great today, thank you for asking {nameRecipient}!`,
+  },
+  {
+    id: 'weather',
+    variations: [
       'weather',
       "how's the weather",
       "how's the weather?",
@@ -48,105 +61,274 @@ export const dialogsSets: DialogsSets = [
       "what's the weather like",
       'weather report',
     ],
-    `The weather is currently sunny.`,
-  ],
-  [
-    [
+    response: `The weather is currently sunny.`,
+  },
+  {
+    id: 'good_morning',
+    variations: [
       'good morning',
       'morning',
       'top of the morning',
       'morning greeting',
       'rise and shine',
     ],
-    `Good morning, {nameRecipient}!`,
-  ],
-  [
-    [
+    response: `Good morning, {nameRecipient}!`,
+  },
+  {
+    id: 'good_evening',
+    variations: [
       'good evening',
       'evening',
       'evening greeting',
       'greeting at night',
       'night greeting',
     ],
-    `Good evening, {nameRecipient}!`,
-  ],
-  [
-    [
+    response: `Good evening, {nameRecipient}!`,
+  },
+  {
+    id: 'how_are_you_doing',
+    variations: [
       'how are you doing',
       "how's it going",
       "what's up",
       "what's happening",
       "how's life",
     ],
-    `I'm doing well, thank you for asking, {nameRecipient}.`,
-  ],
-  [
-    ['thank you', 'thanks', 'appreciate it', 'thanks a lot', 'thankful'],
-    `You're welcome, {nameRecipient}!`,
-  ],
-  [
-    ['goodbye', 'bye', 'farewell', 'see you later', 'take care'],
-    `Goodbye, {nameRecipient}! Take care.`,
-  ],
-  [
-    [
+    response: `I'm doing well, thank you for asking, {nameRecipient}.`,
+  },
+  {
+    id: 'thank_you',
+    variations: [
+      'thank you',
+      'thanks',
+      'appreciate it',
+      'thanks a lot',
+      'thankful',
+    ],
+    response: `You're welcome, {nameRecipient}!`,
+  },
+  {
+    id: 'goodbye',
+    variations: ['goodbye', 'bye', 'farewell', 'see you later', 'take care'],
+    response: `Goodbye, {nameRecipient}! Take care.`,
+  },
+  {
+    id: 'what_is_new',
+    variations: [
       "what's new",
       'any news',
       "what's happening",
       'latest updates',
       "what's going on",
     ],
-    `Nothing much is new, {nameRecipient}.`,
-  ],
-  [
-    [
+    response: `Nothing much is new, {nameRecipient}.`,
+  },
+  {
+    id: 'how_is_work',
+    variations: [
       "how's work",
       'work going well',
       "how's the job",
       'job going well',
       'work update',
     ],
-    `Work is going fine, {nameRecipient}.`,
-  ],
-  [
-    [
+    response: `Work is going fine, {nameRecipient}.`,
+  },
+  {
+    id: 'how_is_family',
+    variations: [
       "how's family",
       'family doing well',
       "how's everyone",
       'family update',
       'family news',
     ],
-    `Family is doing well, {nameRecipient}.`,
-  ],
-  [
-    [
+    response: `Family is doing well, {nameRecipient}.`,
+  },
+  {
+    id: 'what_is_for_dinner',
+    variations: [
       "what's for dinner",
       'dinner plans',
       "what's cooking",
       'meal planning',
       'dinner time',
     ],
-    `I'm not sure yet, {nameRecipient}.`,
-  ],
-  [
-    [
+    response: `I'm not sure yet, {nameRecipient}.`,
+  },
+  {
+    id: 'how_is_your_day',
+    variations: [
       "how's your day",
       'day going well',
       "how's everything",
       'daily check-in',
       "how's your day been",
     ],
-    `My day is going well, {nameRecipient}.`,
-  ],
-  [
-    [
+    response: `My day is going well, {nameRecipient}.`,
+  },
+  {
+    id: 'how_is_school',
+    variations: [
       "how's school",
       'school going well',
       "how's studies",
       'school update',
       'learning going well',
     ],
-    `School is going well, {nameRecipient}.`,
-  ],
-  // Добавьте еще диалоговые наборы при необходимости
+    response: `School is going well, {nameRecipient}.`,
+  },
 ];
+
+// export type DialogsSets = [string[], string][];
+//
+//
+// export const dialogsSets: DialogsSets = [
+//   [
+//     [
+//       'hello',
+//       'hi',
+//       'hey',
+//       'howdy',
+//       'greetings',
+//       'hi there',
+//       'how are you today',
+//       'how are you today?',
+//       'how are you',
+//       'how are you?',
+//     ],
+//     'Hello, {nameRecipient}! How are you today?',
+//   ],
+//   [
+//     [
+//       'time',
+//       'current time',
+//       'what time is it?',
+//       'what time is it',
+//       "what's the time",
+//       "what's the time?",
+//       'current hour',
+//     ],
+//     `The current ${new Date().toTimeString()}.`,
+//   ],
+//   [
+//     [
+//       'mood',
+//       'how are you feeling',
+//       "how's your mood",
+//       "how's your mood?",
+//       'feeling today',
+//       'emotional state',
+//     ],
+//     `I'm feeling great today, thank you for asking!`,
+//   ],
+//   [
+//     [
+//       'weather',
+//       "how's the weather",
+//       "how's the weather?",
+//       'forecast',
+//       "what's the weather like",
+//       'weather report',
+//     ],
+//     `The weather is currently sunny.`,
+//   ],
+//   [
+//     [
+//       'good morning',
+//       'morning',
+//       'top of the morning',
+//       'morning greeting',
+//       'rise and shine',
+//     ],
+//     `Good morning, {nameRecipient}!`,
+//   ],
+//   [
+//     [
+//       'good evening',
+//       'evening',
+//       'evening greeting',
+//       'greeting at night',
+//       'night greeting',
+//     ],
+//     `Good evening, {nameRecipient}!`,
+//   ],
+//   [
+//     [
+//       'how are you doing',
+//       "how's it going",
+//       "what's up",
+//       "what's happening",
+//       "how's life",
+//     ],
+//     `I'm doing well, thank you for asking, {nameRecipient}.`,
+//   ],
+//   [
+//     ['thank you', 'thanks', 'appreciate it', 'thanks a lot', 'thankful'],
+//     `You're welcome, {nameRecipient}!`,
+//   ],
+//   [
+//     ['goodbye', 'bye', 'farewell', 'see you later', 'take care'],
+//     `Goodbye, {nameRecipient}! Take care.`,
+//   ],
+//   [
+//     [
+//       "what's new",
+//       'any news',
+//       "what's happening",
+//       'latest updates',
+//       "what's going on",
+//     ],
+//     `Nothing much is new, {nameRecipient}.`,
+//   ],
+//   [
+//     [
+//       "how's work",
+//       'work going well',
+//       "how's the job",
+//       'job going well',
+//       'work update',
+//     ],
+//     `Work is going fine, {nameRecipient}.`,
+//   ],
+//   [
+//     [
+//       "how's family",
+//       'family doing well',
+//       "how's everyone",
+//       'family update',
+//       'family news',
+//     ],
+//     `Family is doing well, {nameRecipient}.`,
+//   ],
+//   [
+//     [
+//       "what's for dinner",
+//       'dinner plans',
+//       "what's cooking",
+//       'meal planning',
+//       'dinner time',
+//     ],
+//     `I'm not sure yet, {nameRecipient}.`,
+//   ],
+//   [
+//     [
+//       "how's your day",
+//       'day going well',
+//       "how's everything",
+//       'daily check-in',
+//       "how's your day been",
+//     ],
+//     `My day is going well, {nameRecipient}.`,
+//   ],
+//   [
+//     [
+//       "how's school",
+//       'school going well',
+//       "how's studies",
+//       'school update',
+//       'learning going well',
+//     ],
+//     `School is going well, {nameRecipient}.`,
+//   ],
+// ];
