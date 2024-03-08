d2e763a3781ff1fddb508c0fd4b3eb6fe5e3cb5a
sergehall <sergioartg@gmail.com>
2024-03-08 01:53:58 -0800

    add ActivateTelegramBotUseCase

--- src/features/telegram/telegram.module.ts
+++ src/features/telegram/telegram.module.ts
@@ -11,16 +11,39 @@ import { TelegramTextParserUseCase } from './application/use-cases/telegram-text
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TelegramBotStatusEntity } from './entities/telegram-bot-status.entity';
 import { GenerateTelegramActivationLinkUseCase } from './application/use-cases/generate-telegram-activation-code.use-case';
+import { ActivateTelegramBotUseCase } from './application/use-cases/activate-telegram-bot.use-case';
+import { UsersRepo } from '../users/infrastructure/users-repo';
+import { UsersEntity } from '../users/entities/users.entity';
+import { KeyResolver } from '../../common/helpers/key-resolver';
+import { UuidErrorResolver } from '../../common/helpers/uuid-error-resolver';
+import { GamePairsRepo } from '../pair-game-quiz/infrastructure/game-pairs.repo';
+import { PairsGameEntity } from '../pair-game-quiz/entities/pairs-game.entity';
+import { ChallengeQuestionsEntity } from '../pair-game-quiz/entities/challenge-questions.entity';
+import { ChallengesQuestionsRepo } from '../pair-game-quiz/infrastructure/challenges-questions.repo';
+import { GameQuestionsRepo } from '../pair-game-quiz/infrastructure/game-questions.repo';
+import { QuestionsQuizEntity } from '../sa-quiz-questions/entities/questions-quiz.entity';
 
 const telegramUseCases = [
   SendOurWebhookToTelegramUseCase,
   SendMessagesUseCase,
   TelegramTextParserUseCase,
   GenerateTelegramActivationLinkUseCase,
+  ActivateTelegramBotUseCase,
 ];
 
+const helpers = [KeyResolver, UuidErrorResolver];
+
 @Module({
-  imports: [TypeOrmModule.forFeature([TelegramBotStatusEntity]), CqrsModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      UsersEntity,
+      PairsGameEntity,
+      QuestionsQuizEntity,
+      TelegramBotStatusEntity,
+      ChallengeQuestionsEntity,
+    ]),
+    CqrsModule,
+  ],
 
   controllers: [TelegramController],
   providers: [
@@ -28,7 +51,12 @@ const telegramUseCases = [
     TelegramConfig,
     TelegramService,
     TelegramAdapter,
+    UsersRepo,
+    GamePairsRepo,
+    GameQuestionsRepo,
+    ChallengesQuestionsRepo,
     ...telegramUseCases,
+    ...helpers,
   ],
 })
 export class TelegramModule {}
