a7999bbf97a8c37981eecd88585579b00f7c8496
sergehall <sergioartg@gmail.com>
2024-03-08 04:02:45 -0800

    add ManageTelegramBotUseCase

--- src/features/telegram/application/use-cases/send-messages.use-case.ts
+++ src/features/telegram/application/use-cases/send-messages.use-case.ts
@@ -30,7 +30,7 @@ export class SendMessagesUseCase
 
     const telegramUrl = `${TelegramApiEndpointsEnum.Bot}${tokenTelegramBot}/${sendMessage}`;
 
-    if (payloadTelegramMessage.message.text.includes('code=')) {
+    if (payloadTelegramMessage.message.text.includes('start code=')) {
       const answerToRecipient: string = await this.commandBus.execute(
         new ManageTelegramBotCommand(payloadTelegramMessage),
       );
