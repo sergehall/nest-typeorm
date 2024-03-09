175ee6b3199fb52ce0bed12e5d62a3796fd1dcfb
sergehall <sergioartg@gmail.com>
2024-03-09 03:48:25 -0800

    refactoring SendMessagesUseCase

--- src/adapters/telegram/telegram.adapter.ts
+++ src/adapters/telegram/telegram.adapter.ts
@@ -1,37 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
-import { TelegramApiEndpointsEnum } from '../../features/telegram/enums/telegram-api-endpoints.enum';
-import { TelegramConfig } from '../../config/telegram/telegram.config';
+import { CommandBus } from '@nestjs/cqrs';
+import { SendOurWebhookToTelegramCommand } from '../../features/telegram/application/use-cases/send-our-webhook-to-telegram.use-case';
 
 @Injectable()
 export class TelegramAdapter {
-  axiosInstance: AxiosInstance;
-  tokenTelegramBot: string;
-  constructor(private readonly telegramConfig: TelegramConfig) {
-    this.tokenTelegramBot = this.telegramConfig.getTokenTelegram2(
-      'TOKEN_TELEGRAM_IT_INCUBATOR',
-    );
-    this.axiosInstance = axios.create({
-      baseURL: `${TelegramApiEndpointsEnum.Bot}` + `${this.tokenTelegramBot}/`,
-    });
-  }
+  constructor(private readonly commandBus: CommandBus) {}
 
-  async setWebhook() {
-    await this.axiosInstance.post('/setWebhook', {
-      url: `https://nest-typeorm-heroku-fc82366654b2.herokuapp.com/integrations/telegram/webhook`,
-    });
+  async setWebhook(): Promise<void> {
+    await this.commandBus.execute(new SendOurWebhookToTelegramCommand());
   }
 }
-
-// import { Injectable } from '@nestjs/common';
-// import { CommandBus } from '@nestjs/cqrs';
-// import { SendOurWebhookToTelegramCommand } from '../../features/telegram/application/use-cases/send-our-webhook-to-telegram.use-case';
-//
-// @Injectable()
-// export class TelegramAdapter {
-//   constructor(private readonly commandBus: CommandBus) {}
-//
-//   async setWebhook() {
-//     await this.commandBus.execute(new SendOurWebhookToTelegramCommand());
-//   }
-// }
