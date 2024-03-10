f63bae4576fe20161457aee02812b6fc080e9788
sergehall <sergioartg@gmail.com>
2024-03-09 21:12:18 -0800

    refactoring SendMessagesUseCase

--- src/features/telegram/application/use-cases/send-messages.use-case.ts
+++ src/features/telegram/application/use-cases/send-messages.use-case.ts
@@ -23,46 +23,52 @@ export class SendMessagesUseCase
   async execute(command: SendMessagesCommand) {
     const { payloadTelegramMessage } = command;
 
-    if (payloadTelegramMessage.message.text) {
-      const text: string = payloadTelegramMessage.message.text;
-
-      // Check if the text starts with '/start'
-      if (text.startsWith('/start')) {
-        // If the text starts with '/start' but does not contain 'code=', send a new user welcome message
-        if (!text.includes('code=')) {
-          await this.sendNewUserWelcomeMessage(payloadTelegramMessage);
-          return;
-        }
-
-        // If the text starts with '/start' and contains 'code=', proceed with code extraction and activation
-        const code = await this.extractActivationCode(text);
-        if (code) {
-          const answerToRecipient: string = await this.commandBus.execute(
-            new ManageTelegramBotCommand(payloadTelegramMessage),
-          );
-          await this.sendTelegramMessage(
-            payloadTelegramMessage.message.from.id,
-            answerToRecipient,
-          );
-          return;
-        }
-      }
-
-      // If the text does not start with '/start', or does not contain 'code=', execute TelegramTextParserCommand
-      const answerToRecipient = await this.commandBus.execute(
-        new TelegramTextParserCommand(payloadTelegramMessage),
-      );
-      await this.sendTelegramMessage(
-        payloadTelegramMessage.message.from.id,
-        answerToRecipient,
-      );
-    } else {
-      // If the message does not contain text, send a default response
+    if (!payloadTelegramMessage.message.text) {
       await this.sendTelegramMessage(
         payloadTelegramMessage.message.from.id,
         'do not understand you, please try again!',
       );
+      return;
     }
+
+    const text: string = payloadTelegramMessage.message.text;
+
+    // Define a regular expression pattern to match '/start ' followed by any characters
+    const patternStart = /\/start\s+(.+)/;
+    // Execute the regular expression pattern on the text
+    const match = text.match(patternStart);
+
+    if (match) {
+      // If the text starts with '/start' but does not contain 'code=', send a new user welcome message
+      const partAfterStart = match[1];
+
+      if (!partAfterStart.startsWith('code')) {
+        await this.sendNewUserWelcomeMessage(payloadTelegramMessage);
+        return;
+      }
+
+      // If the text starts with '/start' and contains 'code=', proceed with code extraction and activation
+      const code = await this.extractActivationCode(text);
+      if (code) {
+        const answerToRecipient: string = await this.commandBus.execute(
+          new ManageTelegramBotCommand(payloadTelegramMessage),
+        );
+        await this.sendTelegramMessage(
+          payloadTelegramMessage.message.from.id,
+          answerToRecipient,
+        );
+        return;
+      }
+    }
+
+    // If the text does not start with '/start', or does not contain 'code=', execute TelegramTextParserCommand
+    const answerToRecipient = await this.commandBus.execute(
+      new TelegramTextParserCommand(payloadTelegramMessage),
+    );
+    await this.sendTelegramMessage(
+      payloadTelegramMessage.message.from.id,
+      answerToRecipient,
+    );
   }
 
   // Helper function to send a message via Telegram
@@ -89,18 +95,31 @@ export class SendMessagesUseCase
     );
   }
 
-  // Helper function to extract the activation code
   private async extractActivationCode(message: string): Promise<string | null> {
-    if (message.includes('code=')) {
-      const parts = message.split(' ');
-      for (const part of parts) {
-        if (part.includes('code=')) {
-          return part.split('=')[1];
-        }
-      }
+    // Find the index of the word 'code' in the message
+    const codeIndex = message.indexOf('code');
+
+    // If 'code' is found in the message
+    if (codeIndex !== -1) {
+      // Extract the substring starting from 'code' until the end of the message
+      return message.substring(codeIndex);
     }
-    return null;
+
+    return null; // Return null if 'code' is not found in the message
   }
+
+  // // Helper function to extract the activation code
+  // private async extractActivationCode(message: string): Promise<string | null> {
+  //   if (message.includes('code=')) {
+  //     const parts = message.split(' ');
+  //     for (const part of parts) {
+  //       if (part.includes('code=')) {
+  //         return part.split('=')[1];
+  //       }
+  //     }
+  //   }
+  //   return null;
+  // }
 
   // Helper function to get the Telegram API URL
   private async getTelegramUrl(): Promise<string> {
