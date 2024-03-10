665fed27f420c85ff02fb8735c28f125bd7f585f
sergehall <sergioartg@gmail.com>
2024-03-09 22:11:23 -0800

    refactoring SendMessagesUseCase

--- src/features/telegram/application/use-cases/process-telegram-messages.use-case.ts
+++ src/features/telegram/application/use-cases/process-telegram-messages.use-case.ts
@@ -32,6 +32,8 @@ export class ProcessTelegramMessagesUseCase
     }
 
     const text: string = payloadTelegramMessage.message.text;
+    console.log(text, 'text');
+    console.log(text.startsWith('start'), "text.startsWith('start')");
 
     if (text.startsWith('start') || text.startsWith('/start')) {
       // If the text starts with '/start' but does not contain 'code=', send a new user welcome message
