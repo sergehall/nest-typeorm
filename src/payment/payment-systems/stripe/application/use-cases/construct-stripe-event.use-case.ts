1b7718da9deb303f3b334e1e3dfa5b6afa08d732
sergehall <sergioartg@gmail.com>
2024-03-18 23:04:00 -0700

    add ConstructStripeEventUseCase

---
+++ src/payment/payment-systems/stripe/application/use-cases/construct-stripe-event.use-case.ts
@@ -0,0 +1,54 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { InternalServerErrorException, RawBodyRequest } from '@nestjs/common';
+import { Request } from 'express';
+import { StripeAdapter } from '../../adapter/stripe-adapter';
+import { StripeConfig } from '../../../../../config/stripe/stripe.config';
+import Stripe from 'stripe';
+
+export class ConstructStripeEventCommand {
+  constructor(public rawBodyRequest: RawBodyRequest<Request>) {}
+}
+
+@CommandHandler(ConstructStripeEventCommand)
+export class ConstructStripeEventUseCase
+  implements ICommandHandler<ConstructStripeEventCommand>
+{
+  constructor(
+    private readonly stripeConfig: StripeConfig,
+    private readonly stripeAdapter: StripeAdapter,
+  ) {}
+
+  async execute(
+    command: ConstructStripeEventCommand,
+  ): Promise<Stripe.Event | undefined> {
+    const { rawBodyRequest } = command;
+
+    try {
+      if (
+        rawBodyRequest.headers['stripe-signature'] &&
+        rawBodyRequest.rawBody
+      ) {
+        const stripeWebhookSecret =
+          await this.stripeConfig.getStripeWebhookSecret(
+            'STRIPE_WEBHOOK_SECRET',
+          );
+
+        const signature = rawBodyRequest.headers['stripe-signature'];
+
+        const stripeInstance: Stripe =
+          await this.stripeAdapter.createStripeInstance();
+
+        return stripeInstance.webhooks.constructEvent(
+          rawBodyRequest.rawBody,
+          signature,
+          stripeWebhookSecret,
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException(
+        'Error constructStripeEvent' + error.message,
+      );
+    }
+  }
+}
