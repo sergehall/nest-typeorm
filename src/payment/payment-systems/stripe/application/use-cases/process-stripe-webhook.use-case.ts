2dc771cab1471472b04bf58821a58d87734d0dcc
sergehall <sergioartg@gmail.com>
2024-03-19 02:23:04 -0700

    add ProcessStripeChargeSucceededUseCase

--- src/payment/payment-systems/stripe/application/use-cases/process-stripe-webhook.use-case.ts
+++ src/payment/payment-systems/stripe/application/use-cases/process-stripe-webhook.use-case.ts
@@ -4,6 +4,9 @@ import { Request } from 'express';
 import Stripe from 'stripe';
 import { ConstructStripeEventCommand } from './construct-stripe-event.use-case';
 import { ProcessChargeSucceededCommand } from './process-stripe-charge-succeeded.use-case';
+import { StripeAdapter } from '../../adapter/stripe-adapter';
+import { StripeChargeObjectType } from '../../types/stripe-charge-object.type';
+import { StripeCheckoutSessionType } from '../../types/stripe-checkout-session.type';
 
 export class ProcessStripeWebHookCommand {
   constructor(public rawBodyRequest: RawBodyRequest<Request>) {}
@@ -13,7 +16,10 @@ export class ProcessStripeWebHookCommand {
 export class ProcessStripeWebHookUseCase
   implements ICommandHandler<ProcessStripeWebHookCommand>
 {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly stripeAdapter: StripeAdapter,
+  ) {}
 
   async execute(command: ProcessStripeWebHookCommand) {
     const { rawBodyRequest } = command;
@@ -24,6 +30,18 @@ export class ProcessStripeWebHookUseCase
     console.log(event, 'event0');
     try {
       if (event) {
+        const chargeObject = event.data.object;
+        console.log(chargeObject, 'chargeObject');
+        const stripeChargeObject = chargeObject as StripeCheckoutSessionType;
+        const PAYMENT_INTENT_ID = stripeChargeObject.payment_intent;
+        const stripeInstance: Stripe =
+          await this.stripeAdapter.createStripeInstance();
+        const intent = await stripeInstance.paymentIntents.retrieve(
+          `${PAYMENT_INTENT_ID}`,
+        );
+        const latest_charge = intent.latest_charge;
+        console.log(latest_charge, 'latest_charge');
+
         switch (event.type) {
           case 'checkout.session.completed':
             console.log(event, 'event1');
---
+++ src/payment/payment-systems/stripe/types/stripe-checkout-session.type.ts
@@ -0,0 +1,96 @@
+export type StripeCheckoutSessionType = {
+  id: string;
+  object: string;
+  after_expiration: string | null;
+  allow_promotion_codes: boolean | null;
+  amount_subtotal: number;
+  amount_total: number;
+  automatic_tax: {
+    enabled: boolean;
+    liability: string | null;
+    status: string | null;
+  };
+  billing_address_collection: string | null;
+  cancel_url: string;
+  client_reference_id: string;
+  client_secret: string | null;
+  consent: string | null;
+  consent_collection: string | null;
+  created: number;
+  currency: string;
+  currency_conversion: string | null;
+  custom_fields: any[];
+  custom_text: {
+    after_submit: string | null;
+    shipping_address: string | null;
+    submit: string | null;
+    terms_of_service_acceptance: string | null;
+  };
+  customer: string | null;
+  customer_creation: string;
+  customer_details: {
+    address: {
+      city: string | null;
+      country: string;
+      line1: string | null;
+      line2: string | null;
+      postal_code: string | null;
+      state: string | null;
+    };
+    email: string | null;
+    name: string | null;
+    phone: string | null;
+    tax_exempt: string;
+    tax_ids: any[];
+  };
+  customer_email: string | null;
+  expires_at: number;
+  invoice: string | null;
+  invoice_creation: {
+    enabled: boolean;
+    invoice_data: {
+      account_tax_ids: any[] | null;
+      custom_fields: any[] | null;
+      description: string | null;
+      footer: string | null;
+      issuer: string | null;
+      metadata: Record<string, any>;
+      rendering_options: any[] | null;
+    };
+  };
+  livemode: boolean;
+  locale: string | null;
+  metadata: Record<string, any>;
+  mode: string;
+  payment_intent: string;
+  payment_link: string | null;
+  payment_method_collection: string;
+  payment_method_configuration_details: any;
+  payment_method_options: {
+    card: {
+      request_three_d_secure: string;
+    };
+  };
+  payment_method_types: string[];
+  payment_status: string;
+  phone_number_collection: {
+    enabled: boolean;
+  };
+  recovered_from: string | null;
+  setup_intent: string | null;
+  shipping_address_collection: string | null;
+  shipping_cost: number | null;
+  shipping_details: string | null;
+  shipping_options: any[];
+  status: string;
+  submit_type: string | null;
+  subscription: string | null;
+  success_url: string;
+  total_details: {
+    amount_discount: number;
+    amount_shipping: number;
+    amount_tax: number;
+  };
+  ui_mode: string;
+  url: string | null;
+};
