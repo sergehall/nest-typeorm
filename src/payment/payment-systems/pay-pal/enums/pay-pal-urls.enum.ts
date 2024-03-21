44d5f4c9aa574004a1cd3f7189ec01f5594c4bf5
sergehall <sergioartg@gmail.com>
2024-03-21 02:44:53 -0700

    refactoring pay-pal.adapter.ts

--- src/payment/payment-systems/pay-pal/enums/pay-pal-urls.enum.ts
+++ src/payment/payment-systems/pay-pal/enums/pay-pal-urls.enum.ts
@@ -1,5 +1,4 @@
 export enum PayPalUrlsEnum {
   DeveloperAccounts = 'https://developer.paypal.com/developer/accounts',
-  BaseApi = 'https://api-m.paypal.com',
   BaseSandboxApi = 'https://api-m.sandbox.paypal.com',
 }
