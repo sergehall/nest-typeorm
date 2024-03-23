71f1594c130c13c0b141351b7228f73320d2648a
sergehall <sergioartg@gmail.com>
2024-03-22 23:32:14 -0700

    add PayPalCapturePaymentUseCase

--- src/payment/payment-systems/pay-pal/application/use-cases/pay-pal-capture-payment.use-case.ts
+++ src/payment/payment-systems/pay-pal/application/use-cases/pay-pal-capture-payment.use-case.ts
@@ -2,36 +2,32 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import axios from 'axios';
 import { InternalServerErrorException } from '@nestjs/common';
 import { PayPalAdapter } from '../../adapter/pay-pal.adapter';
-import { PayPalFactory } from '../../../../../config/pay-pal/pay-pal-factory';
 
 export class PayPalCapturePaymentCommand {
-  constructor(public id: string) {}
+  constructor(
+    public link: string,
+    public reference_id: string,
+  ) {}
 }
 
 @CommandHandler(PayPalCapturePaymentCommand)
 export class PayPalCapturePaymentUseCase
   implements ICommandHandler<PayPalCapturePaymentCommand>
 {
-  constructor(
-    private readonly payPalAdapter: PayPalAdapter,
-    private readonly payPalFactory: PayPalFactory,
-  ) {}
+  constructor(private readonly payPalAdapter: PayPalAdapter) {}
 
   async execute(command: PayPalCapturePaymentCommand): Promise<string> {
-    const { id } = command;
+    const { link, reference_id } = command;
     try {
       const accessToken =
         await this.payPalAdapter.generateAccessToken('PAYPAL_CLIENT_ID');
 
-      const baseUrl = await this.payPalFactory.getPayPalUrlsDependentEnv();
-      const url = baseUrl + `/v2/checkout/orders/${id}/capture`;
-
       const response = await axios.post(
-        url,
+        link,
         {},
         {
           headers: {
-            'Content-Type': 'application/json',
+            'PayPal-Request-Id': reference_id,
             Authorization: `Bearer ${accessToken}`,
           },
         },
