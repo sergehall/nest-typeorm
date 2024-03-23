b8c08edc68d382f52d11e5ca21de9257bc2cca7e
sergehall <sergioartg@gmail.com>
2024-03-23 01:10:12 -0700

    update PayPalGenerateAccessTokenCommand

--- src/payment/payment-systems/pay-pal/application/use-cases/pay-pal-generate-access-token.use-case.ts
+++ src/payment/payment-systems/pay-pal/application/use-cases/pay-pal-generate-access-token.use-case.ts
@@ -31,7 +31,7 @@ export class PayPalGenerateAccessTokenUseCase
         },
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       });
-
+      console.log(response.data, 'response.data');
       return response.data.access_token;
     } catch (error) {
       console.log(error.message);
