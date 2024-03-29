7d83022f3668e7605762fa171ab87d206b9b50ff
sergehall <sergioartg@gmail.com>
2024-03-28 21:50:52 -0700

    update ProcessPayPalWebhookUseCase

--- src/payment/payment-systems/pay-pal/application/use-cases/finalize-pay-pal-payment.use-case.ts
+++ src/payment/payment-systems/pay-pal/application/use-cases/finalize-pay-pal-payment.use-case.ts
@@ -21,7 +21,7 @@ export class FinalizePayPalPaymentUseCase
     const { body } = command;
 
     try {
-      console.log(body, 'body');
+      console.log(JSON.stringify(body), 'body');
       const { reference_id } = body.resource.purchase_units[0];
       if (!reference_id)
         throw new InternalServerErrorException('Invalid reference ID');
