afa8f82ca29d62dc8d974eb393a212b59a1bc23e
sergehall <sergioartg@gmail.com>
2024-04-02 22:53:29 -0700

    update ReCaptchaGuard

--- src/features/auth/guards/re-captcha.guard.ts
+++ src/features/auth/guards/re-captcha.guard.ts
@@ -1,37 +1,42 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { ReCaptchaConfig } from '../../../config/recaptcha/re-captcha.config';
-import { RecaptchaResponse } from '../../../adapters/recaptcha/types/recaptcha-response.type';
 
 @Injectable()
 export class ReCaptchaGuard extends ReCaptchaConfig implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+
     const reCaptchaToken = request.body.reCaptchaToken; // Assuming the token is sent in the request body
 
-    // Add your secret key here
-    const secretKey = await this.getReCaptchaValue('RECAPTCHA_SECRET_KEY');
+    // If reCaptchaToken is missing or undefined, deny access
+    if (!reCaptchaToken) {
+      return false;
+    }
 
+    const secretKey = await this.getReCaptchaValue('RECAPTCHA_SECRET_KEY');
     const url = 'https://www.google.com/recaptcha/api/siteverify';
 
-    const headersOption = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      params: {
-        body: `secret=${secretKey}&response=${reCaptchaToken}&remoteip=${request.ip}`,
-      },
-    };
+    const formData = new URLSearchParams();
+    formData.append('secret', secretKey);
+    formData.append('response', reCaptchaToken);
+    formData.append('remoteip', request.ip);
 
     try {
-      const response: RecaptchaResponse = await axios.post(
-        url,
-        {},
-        headersOption,
-      );
+      const response = await axios.post(url, formData.toString(), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
 
-      const { success } = response;
+      const { success, score } = response.data;
+
+      if (!success || score < 0.8) {
+        return false;
+      }
 
       // If reCAPTCHA validation is successful, allow access
-      return success;
+      return true;
     } catch (error) {
       // Log or handle error
       console.error('Error occurred while verifying reCAPTCHA token:', error);
