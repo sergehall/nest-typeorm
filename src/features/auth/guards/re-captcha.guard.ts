a9109ab1ad0e007dacba4e1bd07b3411a0c03e49
sergehall <sergioartg@gmail.com>
2024-04-02 22:59:35 -0700

    update ReCaptchaGuard

--- src/features/auth/guards/re-captcha.guard.ts
+++ src/features/auth/guards/re-captcha.guard.ts
@@ -4,33 +4,44 @@ import { ReCaptchaConfig } from '../../../config/recaptcha/re-captcha.config';
 
 @Injectable()
 export class ReCaptchaGuard extends ReCaptchaConfig implements CanActivate {
+  /**
+   * Determines whether the request should be allowed based on the provided reCAPTCHA token.
+   * @param context The execution context.
+   * @returns A boolean indicating whether the request should be allowed.
+   */
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-
-    const reCaptchaToken = request.body.reCaptchaToken; // Assuming the token is sent in the request body
+    const reCaptchaToken = request.body.reCaptchaToken;
 
     // If reCaptchaToken is missing or undefined, deny access
     if (!reCaptchaToken) {
       return false;
     }
 
+    // Retrieve reCAPTCHA secret key from configuration
     const secretKey = await this.getReCaptchaValue('RECAPTCHA_SECRET_KEY');
+
+    // Construct URL for reCAPTCHA verification endpoint
     const url = 'https://www.google.com/recaptcha/api/siteverify';
 
+    // Construct form data for POST request
     const formData = new URLSearchParams();
     formData.append('secret', secretKey);
     formData.append('response', reCaptchaToken);
     formData.append('remoteip', request.ip);
 
     try {
+      // Send POST request to reCAPTCHA verification endpoint
       const response = await axios.post(url, formData.toString(), {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
       });
 
+      // Extract response data
       const { success, score } = response.data;
 
+      // If reCAPTCHA validation fails or score is below threshold, deny access
       if (!success || score < 0.8) {
         return false;
       }
