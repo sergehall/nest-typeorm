c2c9ab6809cae9e117a5a823d62b3552240375b5
sergehall <sergioartg@gmail.com>
2024-04-10 06:04:08 -0700

    refactoring SwaggerModule

--- src/create-app.ts
+++ src/create-app.ts
@@ -89,12 +89,12 @@ function setupSwagger(app: INestApplication): void {
     })
     .setTitle('IT-Incubator API')
     .setDescription(
-      "The Training IT-Incubator API is a versatile RESTful API built with Nests for managing training activities. It offers endpoints for user management, blog creation, post management, commenting, scheduling, and email notifications. Additionally, it integrates seamlessly with payment systems like Stripe and PayPal to facilitate secure transactions. Postgresql is utilized for database storage, and AWS S3 is employed for file storage. Moreover, it includes integration with Telegram . With its modular architecture, it's easy to build and deploy training applications of any scale. <a href='https://it-incubator.io'>Learn more</a>",
+      "The Training IT-Incubator API is a versatile RESTful API built with Nests for managing training activities. It offers endpoints for user management, blog creation, post management, commenting, scheduling, and email notifications. Additionally, it integrates seamlessly with payment systems like Stripe and PayPal to facilitate secure transactions. Postgresql is utilized for database storage, and AWS S3 is employed for file storage. Moreover, it includes integration with Telegram. With its modular architecture, it's easy to build and deploy training applications of any scale. <a href='https://it-incubator.io'>Learn more</a>",
     )
     .setVersion('36.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 }
 
 /**
